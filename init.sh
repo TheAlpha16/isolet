305d2fbfdc80b27e58f6e86291f2d47d0362c0d4
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2024-01-02 22:39:44 +0530

    added kubernetes/init scripts

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 set +x
 
+cd ./kubernetes/init
+kubectl apply -f instance-namespace.yml
+kubectl apply -f db-volume.yml
+kubectl apply -f network-policy.yml
+
 # initialize secrets and configs
-cd ./kubernetes/configuration
+cd ../configuration
 kubectl apply -f app-secrets.yml.local
 kubectl apply -f app-config.yml
 kubectl apply -f roles.yml
