0dadf35fa307d6b683208a21a316c150b895b855
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2024-01-06 18:52:03 +0530

    changed queries to use context timeout

--- ripper/database/queries.go
+++ ripper/database/queries.go
@@ -1,14 +1,25 @@
 package database
 
+import (
+	"context"
+	"time"
+)
+
 func DeleteFlag(userid int, level int) error {
-	if _, err := DB.Query(`DELETE FROM flags WHERE userid = $1 AND level = $2`, userid, level); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if _, err := DB.QueryContext(ctx, `DELETE FROM flags WHERE userid = $1 AND level = $2`, userid, level); err != nil {
 		return err
 	}
 	return nil
 }
 
 func DeleteRunning(userid int, level int) error {
-	if _, err := DB.Query(`DELETE FROM running WHERE userid = $1 AND level = $2`, userid, level); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if _, err := DB.QueryContext(ctx, `DELETE FROM running WHERE userid = $1 AND level = $2`, userid, level); err != nil {
 		return err
 	}
 	return nil
