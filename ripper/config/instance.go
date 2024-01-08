cdf5d28faf7dfb2b60da682f71868e369c6a077d
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2024-01-09 01:20:32 +0530

    removed unnecessary variables in ripper

--- ripper/config/instance.go
+++ ripper/config/instance.go
@@ -2,13 +2,10 @@ package config
 
 import (
 	"os"
-	"strconv"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
-var CONCURRENT_INSTANCES, _ = strconv.Atoi(os.Getenv("CONCURRENT_INSTANCES"))
-var INSTANCE_TIME, _ = strconv.Atoi(os.Getenv("INSTANCE_TIME"))
 var INSTANCE_NAME_SECRET = os.Getenv("INSTANCE_NAME_SECRET")
 var INSTANCE_NAMESPACE = os.Getenv("INSTANCE_NAMESPACE")
 var KUBECONFIG_FILE_PATH = os.Getenv("KUBECONFIG_FILE_PATH")
