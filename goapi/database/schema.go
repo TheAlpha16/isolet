868990d02c154c0ea2dad77cee13d8ba58e880f9
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-06-20 23:22:34 +0530

    updated database schema

--- goapi/database/schema.go
+++ goapi/database/schema.go
@@ -15,6 +15,27 @@ func CreateTables() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	// challenge type
+	_, err = DB.QueryContext(ctx, 
+		`CREATE TYPE chall_type AS ENUM ('static', 'dynamic', 'on-demand'
+	)`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// challenge categories
+	_, err = DB.QueryContext(ctx, 
+		`CREATE TABLE IF NOT EXISTS categories(
+			category_id serial PRIMARY KEY,
+			category_name text NOT NULL UNIQUE
+		)`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// users
 	_, err = DB.QueryContext(ctx, `
 	CREATE TABLE IF NOT EXISTS users(
 		userid bigserial PRIMARY KEY,
@@ -23,13 +44,28 @@ func CreateTables() error {
 		score integer DEFAULT 0,
 		rank integer DEFAULT 3,
 		password VARCHAR(100) NOT NULL,
-		lastsubmission bigint DEFAULT EXTRACT(EPOCH FROM NOW())
+		teamid bigint DEFAULT -1
 	)`)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
+	// teams
+	_, err = DB.QueryContext(ctx, `
+	CREATE TABLE IF NOT EXISTS teams(
+		teamid bigserial PRIMARY KEY,
+		teamname text NOT NULL UNIQUE,
+		captain bigint NOT NULL REFERENCES users(userid),
+		members bigint[] NOT NULL DEFAULT '{}',
+		password VARCHAR(100) NOT NULL,
+	)`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// flags
 	_, err = DB.QueryContext(ctx, `
 	CREATE TABLE IF NOT EXISTS flags(
 		flagid bigserial,
@@ -48,6 +84,24 @@ func CreateTables() error {
 		return err
 	}
 
+	// submssion logs
+	_, err = DB.QueryContext(ctx, `
+	CREATE TABLE IF NOT EXISTS sublogs(
+		sid bigserial PRIMARY KEY,
+		chall_id integer NOT NULL REFERENCES challenges(chall_id),
+		userid bigint NOT NULL REFERENCES users(userid),
+		teamid bigint NOT NULL REFERENCES teams(teamid),
+		flag text NOT NULL,
+		correct boolean NOT NULL,
+		ip inet NOT NULL,
+		subtime timestamp NOT NULL DEFAULT NOW()
+	)`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// running instances
 	_, err = DB.QueryContext(ctx, `
 	CREATE TABLE IF NOT EXISTS running(
 		runid bigserial,
@@ -59,6 +113,7 @@ func CreateTables() error {
 		return err
 	}
 
+	// to verify users
 	_, err = DB.QueryContext(ctx, `
 	CREATE TABLE IF NOT EXISTS toverify(
 		vid bigserial PRIMARY KEY,
@@ -72,20 +127,34 @@ func CreateTables() error {
 		return err
 	}
 
+	// challenges
 	_, err = DB.QueryContext(ctx, `
 	CREATE TABLE IF NOT EXISTS challenges(
 		chall_id serial PRIMARY KEY,
 		level integer NOT NULL UNIQUE,
 		chall_name text NOT NULL,
+		category_id integer NOT NULL REFERENCES categories(category_id),
 		prompt text,
+		flag text,
+		type chall_type NOT NULL DEFAULT 'static',
+		points integer NOT NULL DEFAULT 100,
+		files text[] DEFAULT ARRAY[]::text[],
+		hints text[],
 		solves integer DEFAULT 0,
-		tags text[]
+		author text default 'anonymous',
+		visible boolean DEFAULT false,
+		tags text[],
+		port integer DEFAULT 0,
+		subd text DEFAULT 'localhost',
+		cpu integer DEFAULT 5,
+		mem integer DEFAULT 10
 	)`)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
+	// clear old toverify entries
 	_, err = DB.QueryContext(ctx, fmt.Sprintf(`
 	CREATE OR REPLACE FUNCTION toverify_delete_old_rows() RETURNS trigger
 	LANGUAGE plpgsql
@@ -101,16 +170,7 @@ func CreateTables() error {
 		return err
 	}
 
-	_, err = DB.QueryContext(ctx, `
-	CREATE OR REPLACE TRIGGER toverify_delete_old_rows_trigger
-		BEFORE INSERT ON toverify
-		EXECUTE PROCEDURE toverify_delete_old_rows();
-	`)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
+	// enforce instance count
 	_, err = DB.QueryContext(ctx, fmt.Sprintf(`
 	CREATE OR REPLACE FUNCTION enforce_instance_count() RETURNS trigger
 	LANGUAGE plpgsql
@@ -145,11 +205,50 @@ func CreateTables() error {
 		log.Println(err)
 		return err
 	}
+	
+	// trigger to add captain to members
+	_, err = DB.QueryContext(ctx, `
+	CREATE OR REPLACE FUNCTION add_captain_to_members()
+	RETURNS TRIGGER AS $$
+	BEGIN
+		NEW.members := array_append(NEW.members, NEW.captain);
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
+	`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 
+	// trigger to delete old toverify entries
+	_, err = DB.QueryContext(ctx, `
+	CREATE OR REPLACE TRIGGER toverify_delete_old_rows_trigger
+	BEFORE INSERT ON toverify
+	EXECUTE PROCEDURE toverify_delete_old_rows();
+	`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// trigger to enforce instance count
+	_, err = DB.QueryContext(ctx, `
+	CREATE TRIGGER add_captain_to_members_trigger
+	BEFORE INSERT ON teams
+	FOR EACH ROW
+	EXECUTE FUNCTION add_captain_to_members();
+	`)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// trigger to enforce instance count
 	_, err = DB.QueryContext(ctx, `
 	CREATE OR REPLACE TRIGGER enforce_instance_count_trigger
-    	BEFORE INSERT ON running
-    	FOR EACH ROW EXECUTE PROCEDURE enforce_instance_count();
+	BEFORE INSERT ON running
+	FOR EACH ROW EXECUTE PROCEDURE enforce_instance_count();
 	`)
 	if err != nil {
 		log.Println(err)
