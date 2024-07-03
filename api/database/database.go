7ddec467445ff0eed1ba9dc8fb4d442bc87bc8f7
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-07-03 21:36:38 +0530

    Turn off logging in GORM

--- api/database/database.go
+++ api/database/database.go
@@ -9,8 +9,9 @@ import (
 	"os"
 	// "time"
 
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm/logger"
 
 	"github.com/TheAlpha16/isolet/api/config"
 	"github.com/TheAlpha16/isolet/api/models"
@@ -25,7 +26,9 @@ func Connect() error {
 
 	connConfig := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.DB_HOST, config.DB_PORT, config.DB_USER, config.DB_PASS, config.DB_NAME)
 
-	DB, err = gorm.Open(postgres.Open(connConfig), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(connConfig), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	return err
 	// if err != nil {
 	// 	return err
