94d3760b07becd14b053b152eb45c3d13eb688e9
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-07-03 21:35:18 +0530

    Retrieve DB_PORT dynamically

--- api/config/db.go
+++ api/config/db.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"os"
+	"strconv"
+
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -9,4 +11,12 @@ var DB_USER = os.Getenv("POSTGRES_USER")
 var DB_HOST = os.Getenv("POSTGRES_HOST")
 var DB_PASS = os.Getenv("POSTGRES_PASSWORD")
 var DB_NAME = os.Getenv("POSTGRES_DATABASE")
-var DB_PORT = 5432
+var DB_PORT = getPort()
+
+func getPort() int {
+	port, err := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	if err != nil {
+		return 5432
+	}
+	return port
+}
