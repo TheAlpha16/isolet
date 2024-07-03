ef1f7210ff90f0ffef5f89876a78304723139475
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-07-03 17:43:00 +0530

    Remove Creds struct

--- api/models/models.go
+++ api/models/models.go
@@ -5,19 +5,13 @@ import (
 )
 
 type User struct {
-	UserID	 int	  `json:"userid"`
-	Email 	 string   `json:"email" form:"email"`
-	Username string   `json:"username" form:"username"`
-	Password string   `json:"password" form:"password"`
-	Confirm  string   `json:"confirm" form:"confirm"`
-	Rank	 int      `json:"rank"`
-	TeamID	 int	  `json:"teamid"`
-}
-
-type Creds struct {
-	Email 	 string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-	Username string `json:"username" form:"username"`
+	UserID   uint   `gorm:"primaryKey;autoIncrement" json:"userid"`
+	Email    string `gorm:"unique;not null" json:"email" form:"email"`
+	Username string `gorm:"unique;not null" json:"username" form:"username"`
+	Password string `gorm:"not null" json:"password" form:"password"`
+	Confirm  string `gorm:"-" json:"confirm" form:"confirm"`
+	Rank     int    `gorm:"default:3" json:"rank"`
+	TeamID   int    `gorm:"default:-1" json:"teamid"`
 }
 
 type VerifyClaims struct {
