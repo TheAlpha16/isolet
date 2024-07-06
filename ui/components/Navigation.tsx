863beffab2920fb1d30c96b47221d9177a6ced08
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-07-06 13:12:16 +0530

    Modify nav bar

--- ui/components/Navigation.tsx
+++ ui/components/Navigation.tsx
@@ -1,29 +1,41 @@
-'use client'
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
+import path from "path";
 
-interface Props{
-	loggedin: boolean
+interface Props {
+	loggedin: boolean;
 }
 
 function NavBar(props: Props) {
-	var routes = [
-		{
+	var routes = [];
+
+	if (props.loggedin) {
+		routes.push({
 			path: "/challenges",
 			name: "Challenges",
-		},
-		{
+		});
+		routes.push({
 			path: "/scoreboard",
-			name: "Scoreboard"
-		},
-	]
-
-	if (props.loggedin){
+			name: "Scoreboard",
+		});
+		routes.push({
+			path: "/profile",
+			name: "Profile",
+		});
+		routes.push({
+			path: "/team",
+			name: "Team",
+		});
 		routes.push({
 			path: "/logout",
 			name: "Logout",
 		});
 	} else {
+		routes.push({
+			path: "/register",
+			name: "Register",
+		});
 		routes.push({
 			path: "/login",
 			name: "Login",
@@ -31,27 +43,29 @@ function NavBar(props: Props) {
 	}
 
 	return (
-			<div className="flex p-2 w-full z-[100] bg-palette-300">
-				<Link href={ "/" } className="flex">
-					<img src="/static/assets/isolet.svg" className="align-center px-2"></img>
-				</Link>
+		<div className="flex p-2 w-full z-[100] bg-palette-300">
+			<Link href={"/"} className="flex">
+				<img
+					src="/static/assets/isolet.svg"
+					className="align-center px-2"
+				></img>
+			</Link>
 
-				<nav className="flex gap-2 justify-end relative w-full z-[100]">
-					{routes.map((item, index) => {
-		
-						return (
-							<Link
-								key={item.path}
-								className="px-3 py-2 rounded-md text-sm lg:text-base font-mono relative no-underline duration-300 ease-in text-palette-200"
-								href={item.path}
-							>
-								<span>{item.name}</span>
-							</Link>
-						);
-					})}
-				</nav>
-			</div>
-	)
+			<nav className="flex gap-2 justify-end relative w-full z-[100]">
+				{routes.map((item, index) => {
+					return (
+						<Link
+							key={item.path}
+							className="px-3 py-2 rounded-md text-sm lg:text-base font-mono relative no-underline duration-300 ease-in text-palette-200"
+							href={item.path}
+						>
+							<span>{item.name}</span>
+						</Link>
+					);
+				})}
+			</nav>
+		</div>
+	);
 }
 
-export default NavBar
+export default NavBar;
