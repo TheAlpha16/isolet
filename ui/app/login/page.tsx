68da5849f316ba34e31c2c6b33da5eb32d465923
Jithendra Nadh <101546002+TheAlpha16@users.noreply.github.com>
2024-07-06 15:46:02 +0530

    Login: Remove older showPassword button

--- ui/app/login/page.tsx
+++ ui/app/login/page.tsx
@@ -97,44 +97,43 @@ function Login() {
 	return (
 		<>
 			<div className="flex flex-col gap-1 px-6 pt-6 pb-4 mt-6 font-mono justify-center self-center border-2 border-palette-600 text-palette-100 rounded-md">
-				<div>
-					<div className="grid grid-cols-1 gap-y-4 justify-items-center">
-						<label>Creds please!</label>
+				<div className="grid grid-cols-1 gap-y-4 justify-items-center">
+					<label>Creds please!</label>
+					<input
+						id="email"
+						name="email"
+						placeholder="Email"
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						className={inputClass}
+					></input>
+					<div className="relative w-full">
 						<input
-							id="email"
-							name="email"
-							placeholder="Email"
-							type="text"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							className={inputClass}
+							id="password"
+							name="password"
+							placeholder="Password"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							onKeyDown={(e) => {
+								if (e.key == "Enter") {
+									handleSubmit();
+								}
+							}}
+							className={`${inputClass} w-full`}
 						></input>
-						<div className="relative w-full">
-							<input
-								id="password"
-								name="password"
-								placeholder="Password"
-								type="password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								onKeyDown={(e) => {
-									if (e.key == "Enter") {
-										handleSubmit();
-									}
-								}}
-								className={`${inputClass} w-full`}
-							></input>
-							<ShowPassword />
-						</div>
-						<button
-							type="submit"
-							className="px-5 py-2 relative duration-300 ease-in bg-palette-500 text-black rounded-md hover:bg-palette-400"
-							onClick={handleSubmit}
-						>
-							Login
-						</button>
+						<ShowPassword />
 					</div>
+					<button
+						type="submit"
+						className="px-5 py-2 relative duration-300 ease-in bg-palette-500 text-black rounded-md hover:bg-palette-400"
+						onClick={handleSubmit}
+					>
+						Login
+					</button>
 				</div>
+
 				<Link href={"/register"}>
 					<div className="text-blue-400 text-sm text-center underline">
 						Sign up
