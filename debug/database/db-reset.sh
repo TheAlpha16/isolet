56796868cbe5e457e86f9c2aa2cb82581f358c06
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-10-15 00:20:39 +0530

    fix: inject env vars from script

--- debug/database/db-reset.sh
+++ debug/database/db-reset.sh
@@ -1,6 +1,13 @@
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=1c25d2068cdd9c3452013dc106e3be8a
+
 docker rm -f test_database
 docker rmi -f isolet-database
 
 cd ../../database
 docker build -t isolet-database .
-docker run -d --name test_database -p 5432:5432 isolet-database
+docker run -d --name test_database -p 5432:5432 -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD isolet-database
+
+sleep 3
+cd ../debug/database
+PGPASSWORD="$POSTGRES_PASSWORD" psql -h localhost -p 5432 -U $POSTGRES_USER -d postgres -f "$(dirname "$0")/fake.sql"
