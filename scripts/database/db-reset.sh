be0f372a3f0ce08fb0d7c07d30ecb7238ed520f7
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-11-16 22:43:08 +0530

    fix: path fixed according to scripts directory

--- scripts/database/db-reset.sh
+++ scripts/database/db-reset.sh
@@ -4,10 +4,10 @@ POSTGRES_PASSWORD=1c25d2068cdd9c3452013dc106e3be8a
 docker rm -f test_database
 docker rmi -f isolet-database
 
-cd ../../database
+cd $(dirname "$0")/../../database
 docker build -t isolet-database .
 docker run -d --name test_database -p 5432:5432 -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD isolet-database
 
 sleep 3
-cd ../debug/database
+cd $(dirname "$0")/../scripts/database
 PGPASSWORD="$POSTGRES_PASSWORD" psql -h localhost -p 5432 -U $POSTGRES_USER -d postgres -f "$(dirname "$0")/fake.sql"
