cde8acf9f29f4d349a09bcd2ac1264a8c822b9aa
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-11-18 19:17:17 +0530

    refactor: display login details for debugging

--- ui-new/app/page.tsx
+++ ui-new/app/page.tsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "@/store/authStore";
 
 export default function Home() {
 
-  const { logout } = useAuthStore();
+  const { loggedIn, user, logout } = useAuthStore();
 
   const yep = () => {
     logout()
@@ -14,7 +14,7 @@ export default function Home() {
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -33,6 +33,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          { loggedIn && <li>{ `Welcome ${user?.username} from ${user?.teamid}` }</li> }
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
