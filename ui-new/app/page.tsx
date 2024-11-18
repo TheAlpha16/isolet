c4f3f193689165d584ee88e300fca7d6849fb850
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-11-18 11:05:07 +0530

    feat: add sample functions for testing

--- ui-new/app/page.tsx
+++ ui-new/app/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+
+import showToast, { ToastStatus } from "@/utils/toastHelper";
 import Image from "next/image";
+import { useAuthStore } from "@/store/authStore";
 
 export default function Home() {
+
+  const { logout } = useAuthStore();
+
+  const yep = () => {
+    logout()
+    showToast(ToastStatus.Success, "yes")
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -47,6 +59,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <button onClick={yep}>
+            Click Me
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
