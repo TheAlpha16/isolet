da3dd15338ef93c6f87c2ae6547c3276de0bb333
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-12-08 16:33:43 +0000

    feat: text at the top of team join page

--- ui-new/app/teaminit/page.tsx
+++ ui-new/app/teaminit/page.tsx
@@ -17,48 +17,53 @@ function TeamInit() {
 		"px-4 py-2 w-72 border border-gray-600 rounded-md bg-background text-foreground";
 
 	return (
-		<div>
-			<form
-				onSubmit={(event) => {
-                    event.preventDefault();
-                    const action = (event.nativeEvent as SubmitEvent).submitter?.id || "";
-					handleSubmit(action);
-				}}
-				className="flex flex-col gap-2 justify-center items-center"
-			>
-				<input
-					id="teamname"
-					type="text"
-					name="teamname"
-					placeholder="teamname"
-					value={teamname}
-					onChange={(event) => {
-						setTeamName(event.target.value);
+		<div className="flex flex-col gap-4">
+			<div className="flex justify-center">
+				Please join a team or create a new team to continue.
+			</div>
+			<div>
+				<form
+					onSubmit={(event) => {
+						event.preventDefault();
+						const action = (event.nativeEvent as SubmitEvent).submitter?.id || "";
+						handleSubmit(action);
 					}}
-					className={inputClass}
-					required
-				></input>
-				<input
-					id="password"
-					type="password"
-					name="password"
-					placeholder="password"
-					value={password}
-					onChange={(event) => {
-						setPassword(event.target.value);
-					}}
-					className={inputClass}
-					required
-				></input>
-				<div className="flex gap-2">
-					<FormButton type="submit" id="join">
-						{loading ? "Joining..." : "Join"}
-					</FormButton>
-					<FormButton type="submit" variant="secondary" id="create">
-						{loading ? "Creating..." : "Create"}
-					</FormButton>
-				</div>
-			</form>
+					className="flex flex-col gap-2 justify-center items-center"
+				>
+					<input
+						id="teamname"
+						type="text"
+						name="teamname"
+						placeholder="teamname"
+						value={teamname}
+						onChange={(event) => {
+							setTeamName(event.target.value);
+						}}
+						className={inputClass}
+						required
+					></input>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						placeholder="password"
+						value={password}
+						onChange={(event) => {
+							setPassword(event.target.value);
+						}}
+						className={inputClass}
+						required
+					></input>
+					<div className="flex gap-2">
+						<FormButton type="submit" id="join">
+							{loading ? "Joining..." : "Join"}
+						</FormButton>
+						<FormButton type="submit" variant="secondary" id="create">
+							{loading ? "Creating..." : "Create"}
+						</FormButton>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 }
