ede72db3fc0f150cf08df2df9f4bf126b7db1fe6
TheAlpha16 <uddagirijithendranadh@gmail.com>
2024-12-18 03:42:36 +0000

    ref: move challenge functions to a new file

--- api/handler/api.go
+++ api/handler/api.go
@@ -1,10 +1,8 @@
 package handler
 
 import (
-	"log"
 	"time"
 	"strconv"
-	"strings"
 
 	"github.com/TheAlpha16/isolet/api/database"
 	"github.com/TheAlpha16/isolet/api/models"
@@ -12,55 +10,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 )
-
-
-func GetChalls(c *fiber.Ctx) error {
-	var teamid int64
-
-	claims := c.Locals("user").(*jwt.Token).Claims.(jwt.MapClaims)
-	teamid = int64(claims["teamid"].(float64))
-
-	challenges, err := database.ReadChallenges(c, teamid)
-	if err != nil {
-		log.Println(err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": "failure", "message": "error in reading challenges"})
-	}
-	return c.Status(fiber.StatusOK).JSON(challenges)
-}
-
-func SubmitFlag(c *fiber.Ctx) error {
-	var userid int64
-	var teamid int64
-	var err error
-
-	claims := c.Locals("user").(*jwt.Token).Claims.(jwt.MapClaims)
-	userid = int64(claims["userid"].(float64))
-	teamid = int64(claims["teamid"].(float64))
-
-	chall_id_string := c.FormValue("chall_id")
-	flag := c.FormValue("flag")
-
-	if flag == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "failure", "message": "missing flag in the request"})
-	}
-
-	if chall_id_string == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "failure", "message": "missing chall_id in the request"})
-	}
-
-	chall_id, err := strconv.Atoi(chall_id_string)
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "failure", "message": "invalid chall_id"})
-	}
-
-	flag = strings.TrimSpace(flag)
-
-	if isOK, message := database.VerifyFlag(c, chall_id, userid, teamid, flag); !isOK {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "failure", "message": message})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "correct flag"})
-}
 
 func ShowScoreBoard(c *fiber.Ctx) error {
 	page_string := c.Query("page", "1")
