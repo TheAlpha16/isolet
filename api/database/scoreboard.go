b7a72c4bc4504db9755ce3d264a5f06dee01e98a
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-14 12:05:42 +0000

    fix: modify GetTeamScore fn for new solves table

--- api/database/scoreboard.go
+++ api/database/scoreboard.go
@@ -71,7 +71,8 @@ func GetTeamScore(teamid int64) (int, error) {
 	var score int
 	err := db.Table("teams").
 		Select(`COALESCE(SUM(challenges.points), 0) - teams.cost AS score`).
-		Joins("LEFT JOIN challenges ON challenges.chall_id = ANY(teams.solved)").
+		Joins("LEFT JOIN solves ON solves.teamid = teams.teamid").
+		Joins("LEFT JOIN challenges ON challenges.chall_id = solves.chall_id").
 		Where("teams.teamid = ?", teamid).
 		Group("teams.teamid").
 		Scan(&score).Error
