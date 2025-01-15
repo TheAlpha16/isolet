accc4553997123221b022e81aab6548f04b219de
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-15 07:57:14 +0000

    fix: remove redundant columns in teams model

--- api/models/team.go
+++ api/models/team.go
@@ -5,11 +5,10 @@ import (
 )
 
 type Team struct {
-	TeamID   int64         `gorm:"primaryKey;autoIncrement;column:teamid" json:"teamid"`
-	TeamName string        `gorm:"unique;not null;column:teamname" json:"teamname"`
-	Captain  int64         `gorm:"not null" json:"captain"`
-	Members  pq.Int64Array `gorm:"type:integer[]" json:"members"`
-	Password string        `gorm:"not null" json:"password"`
-	Solved   pq.Int64Array `gorm:"type:integer[]" json:"solved"`
-	UHints   pq.Int64Array `gorm:"type:integer[];column:uhints" json:"uhints"`
+	TeamID         int64         `gorm:"primaryKey;autoIncrement;column:teamid" json:"teamid"`
+	TeamName       string        `gorm:"unique;not null;column:teamname" json:"teamname"`
+	Captain        int64         `gorm:"not null" json:"captain"`
+	Members        pq.Int64Array `gorm:"type:integer[]" json:"members"`
+	Password       string        `gorm:"not null" json:"password"`
+	LastSubmission int64         `gorm:"column:last_submission" json:"last_submission"`
 }
