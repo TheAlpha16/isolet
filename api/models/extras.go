48ba8ac2fa0f54901aa3dbfa1160028890b0fd34
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-18 20:53:33 +0000

    feat: create gorm model for tokens table

--- api/models/extras.go
+++ api/models/extras.go
@@ -1,3 +1,17 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type TeamNameKey struct{}
+
+type Token struct {
+	TID       int64     `gorm:"column:tid;primaryKey;autoIncrement" json:"-"`
+	Token     uuid.UUID `gorm:"column:token;unique;not null;default:gen_random_uuid()" json:"token"`
+	UserID    int64     `gorm:"column:userid;not null" json:"-"`
+	User	  User      `gorm:"foreignKey:UserID;references:UserID" json:"-"`
+	Timestamp time.Time `gorm:"not null;default:CURRENT_TIMESTAMP" json:"-"`
+}
