9e355ed9e7bbbd74f3e9e8882aac271745f7570b
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-21 08:51:35 +0000

    fix: render nav buttons only when menu is open for small screens

--- ui-new/components/NavBar.tsx
+++ ui-new/components/NavBar.tsx
@@ -6,14 +6,14 @@ import { UserRound, LogOut, Trophy, Flag, Menu, X, LogIn, Rocket } from 'lucide-
 import { useState } from "react";
 
 interface Route {
-path: string;
-name: string;
-icon: React.ReactNode;
+	path: string;
+	name: string;
+	icon: React.ReactNode;
 }
 
 function NavBar() {
 	const { loggedIn, fetching, logout } = useAuthStore();
-	const [ menuOpen, setMenuOpen ] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	const routes: Route[] = [
 		{ path: "/profile", name: "Profile", icon: <UserRound size={18} /> },
@@ -23,103 +23,103 @@ function NavBar() {
 
 	return (
 		<>
-		<div className="flex items-center justify-between bg-transparent p-4 font-mono border-b sm:sticky sm:top-0 z-10">
-			<div className="flex items-center gap-4">
-				<Link href="/">
-					<div className="text-foreground text-2xl font-bold">isolet</div>
-				</Link>
+			<div className="flex items-center justify-between bg-transparent p-4 font-mono border-b sm:sticky sm:top-0 z-10">
+				<div className="flex items-center gap-4">
+					<Link href="/">
+						<div className="text-foreground text-2xl font-bold">isolet</div>
+					</Link>
 
-				{loggedIn &&
-					<nav className="sm:flex items-center gap-2 hidden">
-						{routes.map(({ path, name, icon }) => (
-							<Link
-								key={path}
-								className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
-								href={path}
-							>
-								{icon}
-								<span>{name}</span>
-							</Link>
-						))}
-					</nav>
-				}
-			</div>
+					{loggedIn &&
+						<nav className="sm:flex items-center gap-2 hidden">
+							{routes.map(({ path, name, icon }) => (
+								<Link
+									key={path}
+									className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
+									href={path}
+								>
+									{icon}
+									<span>{name}</span>
+								</Link>
+							))}
+						</nav>
+					}
+				</div>
 
-			<div className="items-center gap-4 hidden sm:flex">
+				<div className="items-center gap-4 hidden sm:flex">
+					{!fetching && (
+						<>{loggedIn ? (<>
+							<ThemeToggle />
+							<Button variant="ghost" size="icon" onClick={logout}>
+								<LogOut size={18} />
+								<span className="sr-only">Logout</span>
+							</Button>
+						</>) : (
+							<>
+								<Link href="/register">
+									<Button variant="secondary">Register</Button>
+								</Link>
+								<Link href="/login">
+									<Button>Login</Button>
+								</Link>
+								<ThemeToggle />
+							</>
+						)}</>
+					)}
+				</div>
+
 				{!fetching && (
-					<>{loggedIn ? (<>
+					<div className="flex sm:hidden items-center">
 						<ThemeToggle />
-						<Button variant="ghost" size="icon" onClick={logout}>
-							<LogOut size={18} />
-							<span className="sr-only">Logout</span>
+						<Button variant="ghost" size="icon" onClick={() => setMenuOpen(!menuOpen)}>
+							{menuOpen ? <X size={24} /> : <Menu size={24} />}
+							<span className="sr-only">Menu</span>
 						</Button>
-					</>) : (
-					<>
-						<Link href="/register">
-							<Button variant="secondary">Register</Button>
-						</Link>
-						<Link href="/login">
-							<Button>Login</Button>
-						</Link>
-						<ThemeToggle />
-					</>
-					)}</>
+					</div>
 				)}
 			</div>
 
-			{!fetching && (
-				<div className="flex sm:hidden items-center">
-					<ThemeToggle />
-					<Button variant="ghost" size="icon" onClick={() => setMenuOpen(!menuOpen)}>
-							{menuOpen ? <X size={24} /> : <Menu size={24} />}
-						<span className="sr-only">Menu</span>
-					</Button>
-				</div>
-			)}
-		</div>
-		
-		<div className={`bg-transparent font-mono border-b sm:hidden transition-all duration-300 ease-in-out ${
-          menuOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}>
-			{loggedIn && (
-				<nav className="flex flex-col gap-2 p-4">
-					{routes.map(({ path, name, icon }) => (
-						<Link
-							key={path}
-							className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
-							href={path}
-						>
-							{icon}
-							<span>{name}</span>
-						</Link>
-					))}
-					<Link
-						className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
-						href="#"
-						onClick={logout}
-					>
-						<LogOut size={18} />
-						<span>Logout</span>
-					</Link>
-				</nav>)}
-			{!loggedIn && (
-				<nav className="flex flex-col gap-2 p-4">
-					<Link
-						className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
-						href="/register"
-					>
-						<Rocket size={18} />
-						<span>Register</span>
-					</Link>
-					<Link
-						className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
-						href="/login"
-					>
-						<LogIn size={18} />
-						<span>Login</span>
-					</Link>
-				</nav>
-			)}
-		</div>
+			{menuOpen && (
+				<div className={`bg-transparent font-mono border-b sm:hidden transition-all duration-300 ease-in-out `}>
+					{loggedIn && (
+						<nav className="flex flex-col gap-2 p-4">
+							{routes.map(({ path, name, icon }) => (
+								<Link
+									key={path}
+									className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
+									href={path}
+								>
+									{icon}
+									<span>{name}</span>
+								</Link>
+							))}
+							<Link
+								className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
+								href="#"
+								onClick={logout}
+							>
+								<LogOut size={18} />
+								<span>Logout</span>
+							</Link>
+						</nav>)}
+					{!loggedIn && (
+						<nav className="flex flex-col gap-2 p-4">
+							<Link
+								className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
+								href="/register"
+							>
+								<Rocket size={18} />
+								<span>Register</span>
+							</Link>
+							<Link
+								className="flex items-center gap-2 rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground"
+								href="/login"
+							>
+								<LogIn size={18} />
+								<span>Login</span>
+							</Link>
+						</nav>
+					)}
+				</div>)}
 		</>
 	);
 }
