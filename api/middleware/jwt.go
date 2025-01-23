96f1ae495d82f1962aa2b9338298766e8d962f80
SAGNIK PAL <palsagnik2102@gmail.com>
2025-01-23 10:25:44 +0530

    [chore] package fixes
    Signed-off-by: SAGNIK PAL <palsagnik2102@gmail.com>

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -137,36 +137,3 @@ func GenerateToken(user *models.User) (string, error) {
 	return t, nil
 }
 
-func CheckAdminToken() fiber.Handler {
-	return jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{
-			Key: []byte(config.SESSION_SECRET),
-			JWTAlg: jwtware.HS256,
-		},
-		TokenLookup: "cookie:token",
-
-		// admin only
-		SuccessHandler: func(c *fiber.Ctx) error {
-			user := c.Locals("user").(*jwt.Token)
-			claims := user.Claims.(jwt.MapClaims)
-			userid := int64(claims["userid"].(float64))
-			rank := int64(claims["rank"].(float64))
-			
-			if !database.UserExists(c, userid) {
-				c.ClearCookie("token")
-				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "failure", "message": "user does not exist"})
-			}
-
-			// Check if user is admin (rank == 1)
-			if rank == 1 {
-				return c.Next()
-			}
-
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "failure", "message": "insufficient privileges"})
-		},
-
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "failure", "message": "invalid or expired session token"})
-		},
-	})
-}
