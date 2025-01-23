ed1180912bcc58e727aa3eee8e5fedf23772204c
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-23 03:50:31 +0000

    fix: replace members in gorm models

--- api/models/team.go
+++ api/models/team.go
@@ -1,15 +1,11 @@
 package models
 
-import (
-	"github.com/lib/pq"
-)
-
 type Team struct {
-	TeamID         int64         `gorm:"primaryKey;autoIncrement;column:teamid" json:"teamid"`
-	TeamName       string        `gorm:"unique;not null;column:teamname" json:"teamname"`
-	Captain        int64         `gorm:"not null" json:"captain"`
-	Members        pq.Int64Array `gorm:"type:integer[]" json:"members"`
-	Password       string        `gorm:"not null" json:"password"`
-	Cost		   int64         `gorm:"not null" json:"cost"`
-	LastSubmission int64         `gorm:"column:last_submission" json:"last_submission"`
+	TeamID         int64  `gorm:"primaryKey;autoIncrement;column:teamid" json:"teamid"`
+	TeamName       string `gorm:"unique;not null;column:teamname" json:"teamname"`
+	Captain        int64  `gorm:"not null" json:"captain"`
+	Members        []User `gorm:"-" json:"members"`
+	Password       string `gorm:"not null" json:"password"`
+	Cost           int64  `gorm:"not null" json:"cost"`
+	LastSubmission int64  `gorm:"column:last_submission" json:"last_submission"`
 }
