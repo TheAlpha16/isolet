f3c111201bd53350bcb64da2e21bc7af79ddec53
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-23 03:51:22 +0000

    ref: replace application logic with pg function in join team

--- api/database/team.go
+++ api/database/team.go
@@ -77,32 +77,14 @@ func AuthenticateTeam(c *fiber.Ctx, team *models.Team) error {
 	return nil
 }
 
-func JoinTeam(c *fiber.Ctx, teamname string, userid int64) error {
+func JoinTeam(c *fiber.Ctx, user *models.User, team *models.Team) error {
 	ctx, cancel := context.WithTimeout(c.Context(), 15*time.Second)
 	defer cancel()
 
 	db := DB.WithContext(ctx)
-	team := new(models.Team)
 
-	if err := db.Where("teamname = ?", teamname).First(team).Error; err != nil {
-		log.Println(err)
-		return errors.New("unable to join the team. contact admin")
-	}
-
-	if len(team.Members) >= config.TEAM_LEN {
-		return errors.New("team is full")
-	}
-
-	team.Members = append(team.Members, int64(userid))
-
-	if err := db.Model(&team).Update("members", team.Members).Error; err != nil {
-		log.Println(err)
-		return errors.New("unable to join the team. contact admin")
-	}
-
-	if err := UpdateUserTeam(c, userid, team.TeamID); err != nil {
-		log.Println(err)
-		return errors.New("unable to join the team. contact admin")
+	if err := db.Raw("SELECT join_team(?, ?, ?)", team.TeamID, user.UserID, config.TEAM_LEN).Error; err != nil {
+		return errors.New(CleanSQLException(err.Error()))
 	}
 
 	return nil
