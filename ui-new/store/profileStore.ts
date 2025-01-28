df51dd5fadea049f81789c93799f7d4d78995ca7
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-28 18:58:57 +0000

    [feat] create profile store

--- ui-new/store/profileStore.ts
+++ ui-new/store/profileStore.ts
@@ -1,6 +1,99 @@
-import { TeamType } from "@/utils/types";
+import { create } from "zustand";
+import type { TeamType, ScoreGraphEntryType, CategoryProgress, SubmissionType } from "@/utils/types";
+import showToast, { ToastStatus } from "@/utils/toastHelper";
+import { useScoreboardStore } from "./scoreboardStore";
+import { processScores, processCategoryData } from "@/utils/processScores";
+import { useAuthStore } from "./authStore";
 
-interface ProfileStore {
-	team: TeamType;
-	fetchProfile: () => void;
+interface CorrectVIncorrect {
+	correct: number;
+	incorrect: number;
 }
+interface ProfileStore {
+	teamLoading: boolean;
+	team: TeamType;
+	userSubmissionsProgress: CorrectVIncorrect;
+	teamSubmissionsProgress: CorrectVIncorrect;
+	teamGraph: ScoreGraphEntryType[];
+	userCategoryProgress: CategoryProgress[];
+	teamCategoryProgress: CategoryProgress[];
+	fetchSelfTeam(): void;
+}
+
+export const useProfileStore = create<ProfileStore>((set) => ({
+	teamLoading: true,
+	team: {
+		teamid: -1,
+		teamname: "",
+		members: [],
+		captain: -1,
+		rank: -1,
+		score: -1,
+		submissions: [],
+	},
+	userSubmissionsProgress: {
+		correct: 0,
+		incorrect: 0,
+	},
+	teamSubmissionsProgress: {
+		correct: 0,
+		incorrect: 0,
+	},
+	teamGraph: [],
+	userCategoryProgress: [],
+	teamCategoryProgress: [],
+
+	fetchSelfTeam: async () => {
+		set({ teamLoading: true });
+
+		try {
+			const res = await fetch("/api/profile/team/self");
+
+			if (res.ok) {
+				const response = await res.json();
+
+				const team = response.team;
+				const submissions = response.submissions;
+				const rank = response.rank;
+
+				const teamData = {
+					label: team.teamname,
+					scores: submissions.filter((sub: SubmissionType) => sub.correct === true).map((sub: SubmissionType) => ({
+						timestamp: sub.timestamp,
+						points: sub.points,
+					})),
+				};
+
+				const teamGraph = processScores([teamData], useScoreboardStore.getState().startTime);
+				const userSubmissions = submissions.filter((sub: SubmissionType) => sub.userid === useAuthStore.getState().user.userid);
+
+				var userCorrect = userSubmissions.filter((sub: SubmissionType) => sub.correct === true).length;
+				var userScore = userSubmissions.filter((sub: SubmissionType) => sub.correct === true && sub.userid === useAuthStore.getState().user.userid).reduce((acc: number, sub: SubmissionType) => acc + sub.points, 0);
+
+				var teamCorrect = submissions.filter((sub: SubmissionType) => sub.correct === true).length;
+				var teamScore = submissions.filter((sub: SubmissionType) => sub.correct === true).reduce((acc: number, sub: SubmissionType) => acc + sub.points, 0);
+
+				useAuthStore.getState().user.score = userScore;
+				team.score = teamScore;
+				team.rank = rank;
+
+				const userCategoryProgress = processCategoryData(userSubmissions);
+				const teamCategoryProgress = processCategoryData(submissions);
+
+				set({ team: { ...team, submissions: submissions }, teamGraph: teamGraph, userSubmissionsProgress: { correct: userCorrect, incorrect: userSubmissions.length - userCorrect }, teamSubmissionsProgress: { correct: teamCorrect, incorrect: submissions.length - teamCorrect }, userCategoryProgress: userCategoryProgress, teamCategoryProgress: teamCategoryProgress });
+			} else {
+				const response = await res.json();
+				showToast(ToastStatus.Failure, response.message);
+			}
+
+		} catch (error: any) {
+			if (error.name === "AbortError") {
+				showToast(ToastStatus.Failure, "request timed out!");
+			} else {
+				showToast(ToastStatus.Warning, "seems offline");
+			}
+		} finally {
+			set({ teamLoading: false });
+		}
+	},
+}));
