be6cfb7cc1945db07f850d77dab043b5495e35f6
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-28 16:23:12 +0000

    [fix] add foreign reference to members field

--- api/models/team.go
+++ api/models/team.go
@@ -4,8 +4,8 @@ type Team struct {
 	TeamID         int64    `gorm:"primaryKey;autoIncrement;column:teamid" json:"teamid"`
 	TeamName       string   `gorm:"unique;not null;column:teamname" json:"teamname"`
 	Captain        int64    `gorm:"not null" json:"captain"`
-	Members        []User   `gorm:"column:members" json:"members"`
+	Members        []User   `gorm:"foreignKey:TeamID;references:TeamID" json:"members"`
 	Password       string   `gorm:"not null" json:"-"`
-	Cost           int64    `gorm:"not null" json:"cost"`
-	LastSubmission int64    `gorm:"column:last_submission" json:"last_submission"`
+	Cost           int64    `gorm:"not null" json:"-"`
+	LastSubmission int64    `gorm:"column:last_submission" json:"-"`
 }
