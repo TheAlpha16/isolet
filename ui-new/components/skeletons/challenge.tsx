8e7a0fcff50be0145f20d0abc410d69c8940bc9c
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-01-30 18:19:40 +0000

    [ref] change chall card count to 8

--- ui-new/components/skeletons/challenge.tsx
+++ ui-new/components/skeletons/challenge.tsx
@@ -8,7 +8,7 @@ export function ChallengeSkeleton() {
             <Skeleton className="mb-4 h-10 rounded-md" />
 
             <div className="flex flex-col w-full items-center sm:items-start flex-wrap gap-4 sm:flex-row">
-                {Array.from({ length: 6 }).map((_, i) => (
+                {Array.from({ length: 8 }).map((_, i) => (
                     <Skeleton key={i} className="h-[200px] w-[300px] rounded-md" />
                 ))}
             </div>
