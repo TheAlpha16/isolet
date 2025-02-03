d464a4b0e34d32aa7453a8463330428f4c5bd6e3
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-03 13:29:09 +0000

    [feat] create hook for team invite

---
+++ ui-new/hooks/useInviteToken.ts
@@ -0,0 +1,41 @@
+import fetchTimeout from "@/utils/fetchTimeOut";
+import { useState } from "react";
+import showToast, { ToastStatus } from "@/utils/toastHelper";
+
+function useInvite() {
+    const [inviteToken, setInviteToken] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const generateInviteTokenAPI = async () => {
+        setLoading(true); 
+
+        try {
+            const response = await fetchTimeout(
+                "api/profile/team/invite",
+                5000,
+                new AbortController().signal,
+                {},
+            );
+
+            if (response.ok) {
+                const { token } = await response.json();
+                setInviteToken(token);
+            }
+        } catch (error: any) {
+            if (error.name === "AbortError") {
+                showToast(
+                    ToastStatus.Failure,
+                    "verification timed out, reload!"
+                );
+            } else {
+                showToast(ToastStatus.Warning, "server seems offline");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { inviteToken, loading, generateInviteTokenAPI };
+}
+
+export default useInvite;
