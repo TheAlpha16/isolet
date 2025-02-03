6ae173718c217acae5f3bec684d68eda17ea9050
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-03 13:24:36 +0000

    [feat] create handler function to generate invite token

--- api/handler/profile.go
+++ api/handler/profile.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"fmt"
+
+	"github.com/TheAlpha16/isolet/api/config"
 	"github.com/TheAlpha16/isolet/api/database"
 
 	"github.com/gofiber/fiber/v2"
@@ -34,5 +37,22 @@ func GetSelfTeam(c *fiber.Ctx) error {
 		"submissions": submissions,
 		"rank":        rank,
 		"score":       score,
+	})
+}
+
+func GetInviteToken(c *fiber.Ctx) error {
+	var userid int64
+
+	claims := c.Locals("user").(*jwt.Token).Claims.(jwt.MapClaims)
+	userid = int64(claims["userid"].(float64))
+
+	token, err := database.GenerateInviteToken(c, userid)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": "failure", "message": err.Error()})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status": "success",
+		"token":  fmt.Sprintf("http://%s/onboard/team/invite?token=%s", config.PUBLIC_URL, token),
 	})
 }
