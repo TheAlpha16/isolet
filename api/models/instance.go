b085db4cdeb8277b7db13e34b06f923d7cb2bd86
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-07 08:46:09 +0000

    [feat] add non gorm deployment field in flag model

--- api/models/instance.go
+++ api/models/instance.go
@@ -1,15 +1,16 @@
 package models
 
 type Flag struct {
-	FlagID   int64  `gorm:"primaryKey;autoIncrement;column:flagid" json:"-"`
-	TeamID   int64  `gorm:"not null;column:teamid" json:"-"`
-	ChallID  int    `gorm:"not null;column:chall_id" json:"chall_id"`
-	Flag     string `gorm:"not null" json:"-"`
-	Password string `gorm:"type:text" json:"password"`
-	Port     int    `gorm:"type:integer" json:"port"`
-	Hostname string `gorm:"type:text" json:"hostname"`
-	Deadline int64  `gorm:"type:integer" json:"deadline"`
-	Extended int    `gorm:"type:integer;default:1" json:"-"`
+	FlagID     int64  `gorm:"primaryKey;autoIncrement;column:flagid" json:"-"`
+	TeamID     int64  `gorm:"not null;column:teamid" json:"-"`
+	ChallID    int    `gorm:"not null;column:chall_id" json:"chall_id"`
+	Flag       string `gorm:"not null" json:"-"`
+	Password   string `gorm:"type:text" json:"password"`
+	Port       int    `gorm:"type:integer" json:"port"`
+	Hostname   string `gorm:"type:text" json:"hostname"`
+	Deadline   int64  `gorm:"type:integer" json:"deadline"`
+	Extended   int    `gorm:"type:integer;default:1" json:"-"`
+	Deployment string `gorm:"-" json:"deployment"`
 }
 
 type Running struct {
@@ -27,7 +28,6 @@ type Instance struct {
 	Deployment string `gorm:"column:deployment" json:"-"`
 	ConnString string `gorm:"-" json:"connstring"`
 }
-
 
 type Image struct {
 	IID        int    `gorm:"primaryKey;autoIncrement;column:iid" json:"iid"`
