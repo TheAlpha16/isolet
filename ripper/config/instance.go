24912870093aa56cd4f85b81ac70bc33e31d6054
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-19 05:07:45 +0000

    [feat] read worker count from automation config

--- ripper/config/instance.go
+++ ripper/config/instance.go
@@ -9,3 +9,10 @@ import (
 var INSTANCE_NAME_SECRET = os.Getenv("INSTANCE_NAME_SECRET")
 var INSTANCE_NAMESPACE = os.Getenv("INSTANCE_NAMESPACE")
 var KUBECONFIG_FILE_PATH = os.Getenv("KUBECONFIG_FILE_PATH")
+var WORKER_COUNT = os.Getenv("WORKER_COUNT")
+
+func init() {
+	if WORKER_COUNT == "" {
+		WORKER_COUNT = "5"
+	}
+}
