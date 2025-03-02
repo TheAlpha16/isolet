e25a0803db984489c3b8c01de54a9a4fe1e6b9f9
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-03-02 11:08:22 +0000

    [fix] fix alignemt in routes

--- api/router/routes.go
+++ api/router/routes.go
@@ -10,38 +10,38 @@ import (
 var (
 	PING string = "/ping"
 
-	AUTH_GROUP string = "/auth"
-	LOGIN string = "/login"
-	REGISTER string = "/register"
-	VERIFY string = "/verify"
+	AUTH_GROUP      string = "/auth"
+	LOGIN           string = "/login"
+	REGISTER        string = "/register"
+	VERIFY          string = "/verify"
 	FORGOT_PASSWORD string = "/forgot-password"
-	RESET_PASSWORD string = "/reset-password"
-	METADATA string = "/metadata"
+	RESET_PASSWORD  string = "/reset-password"
+	METADATA        string = "/metadata"
 
-	ONBOARD_GROUP = "/onboard"
-	TEAM_CREATE string = "/team/create"
-	TEAM_JOIN string = "/team/join"
+	ONBOARD_GROUP           = "/onboard"
+	TEAM_CREATE      string = "/team/create"
+	TEAM_JOIN        string = "/team/join"
 	JOIN_TEAM_INVITE string = "/team/invite"
 
 	API_GROUP string = "/api"
-	IDENTIFY string = "/identify"
-	LOGOUT string = "/logout"
+	IDENTIFY  string = "/identify"
+	LOGOUT    string = "/logout"
 
-	CHALLS string = "/challs"
-	SUBMIT string = "/submit"
+	CHALLS      string = "/challs"
+	SUBMIT      string = "/submit"
 	UNLOCK_HINT string = "/hint/unlock"
 
 	SCOREBOARD_GROUP string = "/scoreboard"
-	SHOW_SCOREBOARD string = "/"
-	TOP_SCORES string = "/top"
+	SHOW_SCOREBOARD  string = "/"
+	TOP_SCORES       string = "/top"
 
 	LAUNCH string = "/launch"
-	STOP string = "/stop"
+	STOP   string = "/stop"
 	STATUS string = "/status"
 	EXTEND string = "/extend"
 
-	PROFILE_GROUP string = "/profile"
-	GET_SELF_TEAM string = "/team/self"
+	PROFILE_GROUP    string = "/profile"
+	GET_SELF_TEAM    string = "/team/self"
 	GET_INVITE_TOKEN string = "/team/invite"
 )
 
