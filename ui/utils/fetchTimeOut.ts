864f5965dba19040e289a9d494a1df860fe4ddac
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-03-06 07:53:31 +0000

    [fix] added robots.txt and removed timeout from fetchTimeout

--- ui/utils/fetchTimeOut.ts
+++ ui/utils/fetchTimeOut.ts
@@ -1,11 +1,13 @@
 function fetchTimeout(url: string, ms: number, signal: AbortSignal, options = {}) {
     const controller = new AbortController()
-    const promise = fetch(url, { signal: controller.signal, ...options })
+    // const promise = fetch(url, { signal: controller.signal, ...options })
 
     if (signal) signal.addEventListener("abort", () => controller.abort(), true)
     
-    const timeout = setTimeout(() => controller.abort(), ms)
-    return promise.finally(() => clearTimeout(timeout))
+    // const timeout = setTimeout(() => controller.abort(), ms)
+    // return promise.finally(() => clearTimeout(timeout))
+
+    return fetch(url, { signal: controller.signal, ...options })
 }
 
 export default fetchTimeout;
