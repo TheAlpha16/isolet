1d3b1d475c66cc8543cff125066025cd52f39d56
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-03-06 14:54:42 +0000

    [fix] fine tune middlware checks in api routes

--- api/router/api.go
+++ api/router/api.go
@@ -8,24 +8,24 @@ import (
 )
 
 func SetupAPIRoutes(app *fiber.App) {
-	api := app.Group(API_GROUP, middleware.CheckToken(), middleware.CheckTime(), RateLimiter())
-	api.Get(CHALLS, handler.GetChalls)
-	api.Post(SUBMIT, handler.SubmitFlag)
-	api.Post(UNLOCK_HINT, handler.UnlockHint)
+	api := app.Group(API_GROUP, middleware.CheckToken())
+	api.Get(CHALLS, middleware.CheckTime(), RateLimiter(), handler.GetChalls)
+	api.Post(SUBMIT, middleware.CheckTime(), RateLimiter(), handler.SubmitFlag)
+	api.Post(UNLOCK_HINT, middleware.CheckTime(), RateLimiter(), handler.UnlockHint)
 
-	scoreboard := api.Group(SCOREBOARD_GROUP)
+	scoreboard := api.Group(SCOREBOARD_GROUP, RateLimiter())
 	scoreboard.Get(SHOW_SCOREBOARD, handler.ShowScoreBoard)
 	scoreboard.Get(TOP_SCORES, handler.GetScoreGraph)
 	
-	api.Get(IDENTIFY, handler.Identify)
-	api.Get(LOGOUT, handler.Logout)
+	api.Get(IDENTIFY, RateLimiter(), handler.Identify)
+	api.Get(LOGOUT, RateLimiter(), handler.Logout)
 
-	api.Post(LAUNCH, handler.StartInstance)
-	api.Post(STOP, handler.StopInstance)
-	api.Get(STATUS, handler.GetStatus)
-	api.Post(EXTEND, handler.ExtendTime)
+	api.Post(LAUNCH, middleware.CheckTime(), RateLimiter(), handler.StartInstance)
+	api.Post(STOP, middleware.CheckTime(), RateLimiter(), handler.StopInstance)
+	api.Get(STATUS, middleware.CheckTime(), RateLimiter(), handler.GetStatus)
+	api.Post(EXTEND, middleware.CheckTime(), RateLimiter(), handler.ExtendTime)
 
-	profile := api.Group(PROFILE_GROUP)
+	profile := api.Group(PROFILE_GROUP, RateLimiter())
 	profile.Get(GET_SELF_TEAM, handler.GetSelfTeam)
 	profile.Get(GET_INVITE_TOKEN, handler.GetInviteToken)
 }
