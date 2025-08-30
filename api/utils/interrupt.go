68bc417dc8a7195f73881fa49ac252c8f392ba7b
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-08-30 17:26:45 +0530

    feat (api): create interrupt handler

---
+++ api/utils/interrupt.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var InterruptHandlerChannel chan func() = make(chan func(), 5)
+
+func InterruptHandler() {
+	var interruptHandlers []func()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	func() {
+		for {
+			select {
+			case handler := <-InterruptHandlerChannel:
+				interruptHandlers = append(interruptHandlers, handler)
+			case msg := <-c:
+				close(InterruptHandlerChannel)
+				fmt.Printf("%s\n", msg)
+				return
+			}
+		}
+	}()
+
+	fmt.Println("Gracefully shutting down...")
+	for _, handler := range interruptHandlers {
+		handler()
+	}
+	fmt.Println("Server was successful shutdown.")
+}
