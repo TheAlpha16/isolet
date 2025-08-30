3af603640d86f3321dad2c9891978ce181501a2e
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-08-30 18:20:04 +0530

    ref (api): streamline logger initialization and remove unused dependencies

--- api/utils/logger/logger.go
+++ api/utils/logger/logger.go
@@ -2,11 +2,10 @@ package logger
 
 import (
 	"context"
-	"fmt"
+	"sync"
 
 	"github.com/TheAlpha16/isolet/api/utils"
 
-	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -15,91 +14,54 @@ type contextKey string
 
 const loggerKey contextKey = "logger"
 
-var logger *StandardLogger
+var (
+	logger *StandardLogger
+	once   sync.Once
+)
 
 type StandardLogger struct {
 	*zap.Logger
 }
 
-func Init(config *utils.Config) {
-	if logger != nil {
-		return
-	}
-
-	var cfg zap.Config
-	outputLevel, err := zapcore.ParseLevel(config.LogLevel)
-	if err != nil {
-		outputLevel = zapcore.InfoLevel
-	}
-
-	if config.Environment != utils.LOCAL {
-		cfg = zap.NewProductionConfig()
-		cfg.Level = zap.NewAtomicLevelAt(outputLevel)
-		cfg.Encoding = "json"
-	} else {
-		cfg = zap.NewDevelopmentConfig()
-	}
-
-	cfg.OutputPaths = []string{"stdout"}
-	cfg.ErrorOutputPaths = []string{"stdout"}
-	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	cfg.EncoderConfig.TimeKey = "time"
-	cfg.DisableStacktrace = true
-
-	zapLogger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
-
-	logger = &StandardLogger{zapLogger}
-}
-
-func IntercepterLogger(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
-	f := make([]zap.Field, 0, len(fields)/2)
-
-	for i := 0; i < len(fields); i += 2 {
-		key := fields[i]
-		value := fields[i+1]
-
-		switch v := value.(type) {
-		case string:
-			f = append(f, zap.String(key.(string), v))
-		case int:
-			f = append(f, zap.Int(key.(string), v))
-		case bool:
-			f = append(f, zap.Bool(key.(string), v))
-		default:
-			f = append(f, zap.Any(key.(string), v))
+func Init() {
+	once.Do(func() {
+		var cfg zap.Config
+		config := utils.GetConfig()
+		outputLevel, err := zapcore.ParseLevel(config.LogLevel)
+		if err != nil {
+			outputLevel = zapcore.InfoLevel
 		}
-	}
 
-	logger := FromContext(ctx).With(f...)
+		if config.Environment != utils.LOCAL {
+			cfg = zap.NewProductionConfig()
+			cfg.Level = zap.NewAtomicLevelAt(outputLevel)
+			cfg.Encoding = "json"
+		} else {
+			cfg = zap.NewDevelopmentConfig()
+		}
 
-	switch lvl {
-	case logging.LevelDebug:
-		logger.Debug(msg)
-	case logging.LevelInfo:
-		logger.Info(msg)
-	case logging.LevelWarn:
-		logger.Warn(msg)
-	case logging.LevelError:
-		logger.Error(msg)
-	default:
-		panic(fmt.Sprintf("unknown level %v", lvl))
-	}
+		cfg.OutputPaths = []string{"stdout"}
+		cfg.ErrorOutputPaths = []string{"stdout"}
+		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		cfg.EncoderConfig.TimeKey = "time"
+		cfg.DisableStacktrace = true
+
+		zapLogger, err := cfg.Build()
+		if err != nil {
+			panic(err)
+		}
+
+		logger = &StandardLogger{zapLogger}
+	})
 }
 
 func (l *StandardLogger) WithFields(fields ...zapcore.Field) *StandardLogger {
-	zapLogger := l.Logger
-	for _, field := range fields {
-		zapLogger = zapLogger.With(field)
-	}
-	return &StandardLogger{zapLogger}
+	return &StandardLogger{l.Logger.With(fields...)}
 }
 
 func FromContext(ctx context.Context) *StandardLogger {
-	if logger, ok := ctx.Value(loggerKey).(*StandardLogger); ok {
-		return logger
+	if ctxLogger, ok := ctx.Value(loggerKey).(*StandardLogger); ok {
+		return ctxLogger
 	}
 	return logger
 }
@@ -109,4 +71,8 @@ func NewContext(
 	l *StandardLogger,
 ) context.Context {
 	return context.WithValue(ctx, loggerKey, l)
+}
+
+func GetAppLogger() *StandardLogger {
+	return logger
 }
