7d907ba8dc4bb1f2fcf689b837873b3626fc6346
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-01 01:42:22 +0530

    fix (db): update users table schema to remove default values for team_id and timestamps

--- database/new.sql
+++ database/new.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS users(
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    team_id BIGINT DEFAULT -1,
+    team_id BIGINT,
     role user_role DEFAULT 'player',
     is_verified BOOLEAN DEFAULT FALSE,
     is_banned BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    created_at BIGINT NOT NULL,
+    updated_at BIGINT NOT NULL
 );
