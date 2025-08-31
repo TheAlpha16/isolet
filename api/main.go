c524f0156c971362a62feda7e4aff4063ed08ee5
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-08-31 14:51:59 +0530

    ref (api): move db connection helper to main

--- api/main.go
+++ api/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	errorDom "github.com/TheAlpha16/isolet/api/internal/domain/errors"
 	"github.com/TheAlpha16/isolet/api/internal/repository"
 	"github.com/TheAlpha16/isolet/api/internal/usecase"
 	"github.com/TheAlpha16/isolet/api/utils"
 	"github.com/TheAlpha16/isolet/api/utils/logger"
+	"github.com/TheAlpha16/isolet/api/utils/postgres"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
 
@@ -23,6 +27,7 @@ func main() {
 	// Initialize database connection
 	dbPool, closeDBConn, err := ConnectToDatabase(ctx)
 	if err != nil {
+		errorDom.RaiseToSentry(ctx, err)
 		appLogger.Fatal("Failed to connect to database", zap.Error(err))
 	}
 	defer closeDBConn()
@@ -32,4 +37,22 @@ func main() {
 
 	// Initialize usecases
 	_ = usecase.New(repos)
+}
+
+func ConnectToDatabase(ctx context.Context) (*pgxpool.Pool, func(), error) {
+	config := utils.GetConfig()
+	dbURI := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		config.Database.User,
+		config.Database.Password,
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.Name,
+	)
+
+	dbPool, closeConn, err := postgres.NewConnection(ctx, dbURI)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return dbPool, closeConn, nil
 }
