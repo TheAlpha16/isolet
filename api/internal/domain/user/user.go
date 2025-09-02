93d5653da227ad4ac9eb9a4534a00536ab809838
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-02 23:10:27 +0530

    ref: remove IsVerified field from User struct for consistency

--- api/internal/domain/user/user.go
+++ api/internal/domain/user/user.go
@@ -5,13 +5,12 @@ import (
 )
 
 type User struct {
-	ID         int64
-	Username   string
-	Email      string
-	Password   string
-	TeamID     *int64
-	Role       Role
-	IsVerified bool
-	IsBanned   bool
+	ID       int64
+	Username string
+	Email    string
+	Password string
+	TeamID   *int64
+	Role     Role
+	IsBanned bool
 	domain.BaseEntity
 }
--- api/internal/repository/user/model.go
+++ api/internal/repository/user/model.go
@@ -12,13 +12,12 @@ import (
 
 type User struct {
 	postgres.BaseModel
-	Username   string `gorm:"uniqueIndex;not null"`
-	Email      string `gorm:"uniqueIndex;not null"`
-	Password   string `gorm:"not null"`
-	TeamID     *int64 `gorm:"column:team_id"`
-	Role       string `gorm:"type:user_role;default:'player';not null"`
-	IsVerified bool   `gorm:"default:false;not null"`
-	IsBanned   bool   `gorm:"default:false;not null"`
+	Username string `gorm:"uniqueIndex;not null"`
+	Email    string `gorm:"uniqueIndex;not null"`
+	Password string `gorm:"not null"`
+	TeamID   *int64 `gorm:"column:team_id"`
+	Role     string `gorm:"type:user_role;default:'player';not null"`
+	IsBanned bool   `gorm:"default:false;not null"`
 }
 
 func (u *User) ToDomain(ctx context.Context) (*userDom.User, error) {
@@ -34,25 +33,23 @@ func (u *User) ToDomain(ctx context.Context) (*userDom.User, error) {
 			CreatedAt: time.Unix(u.CreatedAt, 0),
 			UpdatedAt: time.Unix(u.UpdatedAt, 0),
 		},
-		ID:         u.ID,
-		Username:   u.Username,
-		Email:      u.Email,
-		Password:   u.Password,
-		TeamID:     u.TeamID,
-		Role:       role,
-		IsVerified: u.IsVerified,
-		IsBanned:   u.IsBanned,
+		ID:       u.ID,
+		Username: u.Username,
+		Email:    u.Email,
+		Password: u.Password,
+		TeamID:   u.TeamID,
+		Role:     role,
+		IsBanned: u.IsBanned,
 	}, nil
 }
 
 func NewUserModel(u *userDom.User) (*User, error) {
 	return &User{
-		Username:   u.Username,
-		Email:      u.Email,
-		Password:   u.Password,
-		TeamID:     u.TeamID,
-		Role:       string(u.Role),
-		IsVerified: u.IsVerified,
-		IsBanned:   u.IsBanned,
+		Username: u.Username,
+		Email:    u.Email,
+		Password: u.Password,
+		TeamID:   u.TeamID,
+		Role:     string(u.Role),
+		IsBanned: u.IsBanned,
 	}, nil
 }
