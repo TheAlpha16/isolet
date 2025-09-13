de286a91a3bf9702003af16cc08484f435b73974
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-14 00:06:18 +0530

    feat: inject infra struct into delivery layer

--- api/delivery/rest/rest.go
+++ api/delivery/rest/rest.go
@@ -7,6 +7,7 @@ import (
 	healthHan "github.com/TheAlpha16/isolet/api/delivery/rest/handler/health"
 	"github.com/TheAlpha16/isolet/api/delivery/rest/middleware"
 	"github.com/TheAlpha16/isolet/api/delivery/rest/routes"
+	"github.com/TheAlpha16/isolet/api/infra"
 	"github.com/TheAlpha16/isolet/api/internal/usecase"
 	"github.com/TheAlpha16/isolet/api/utils"
 	"github.com/TheAlpha16/isolet/api/utils/logger"
@@ -16,7 +17,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func New(usecases *usecase.Usecases) *fiber.App {
+func New(usecases *usecase.Usecases, infra *infra.Infra) *fiber.App {
 	config := utils.GetConfig()
 	app := fiber.New(
 		fiber.Config{
