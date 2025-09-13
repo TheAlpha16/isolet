463838a93004738776bce41c7688e43d43753df8
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-13 21:39:56 +0530

    ref: rename errors module in jwt package

--- api/infra/jwt/jwt.go
+++ api/infra/jwt/jwt.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/TheAlpha16/isolet/api/internal/domain/common"
-	"github.com/TheAlpha16/isolet/api/internal/domain/errors"
+	errorDom "github.com/TheAlpha16/isolet/api/internal/domain/errors"
 	"github.com/lestrrat-go/jwx/v3/jwa"
 	"github.com/lestrrat-go/jwx/v3/jwt"
 )
@@ -19,13 +19,13 @@ func (jwtImpl *jwtImpl) Sign(ctx context.Context, claims *Claims) (string, error
 
 	jwtToken, err := claims.ToJWTToken(ctx)
 	if err != nil {
-		return "", errors.RaiseInternal(ctx, "", err, extraData)
+		return "", errorDom.RaiseInternal(ctx, "", err, extraData)
 	}
 	extraData["jwt"] = jwtToken
 
 	signedToken, err := jwt.Sign(jwtToken, jwt.WithKey(jwtImpl.algorithm, jwtImpl.key))
 	if err != nil {
-		return "", errors.Raise(ctx, errors.ErrTokenSigningFailed, "", err, extraData)
+		return "", errorDom.Raise(ctx, errorDom.ErrTokenSigningFailed, "", err, extraData)
 	}
 	return string(signedToken), nil
 }
@@ -35,12 +35,12 @@ func (jwtImpl *jwtImpl) Verify(ctx context.Context, token string) (*Claims, erro
 
 	jwtToken, err := jwt.ParseString(token, jwt.WithKey(jwtImpl.algorithm, jwtImpl.key))
 	if err != nil {
-		return nil, errors.Raise(ctx, errors.ErrTokenExpiredInvalid, "", err, extraData)
+		return nil, errorDom.Raise(ctx, errorDom.ErrTokenExpiredInvalid, "", err, extraData)
 	}
 
 	claims, err := JWTTokenToClaims(ctx, jwtToken)
 	if err != nil {
-		return nil, errors.Raise(ctx, errors.ErrTokenExpiredInvalid, "", err, extraData)
+		return nil, errorDom.Raise(ctx, errorDom.ErrTokenExpiredInvalid, "", err, extraData)
 	}
 
 	return claims, nil
