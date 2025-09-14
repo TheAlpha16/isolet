f7e807cb37b9fa56d1fe52fd03178fd9f1f6cd7c
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-14 17:55:16 +0530

    feat: add team_id field in auth session

--- api/internal/domain/auth/auth.go
+++ api/internal/domain/auth/auth.go
@@ -2,6 +2,7 @@ package auth
 
 type Session struct {
 	UserID    int64  `json:"user_id"`
+	TeamID    *int64 `json:"team_id,omitempty"`
 	Token     string `json:"-"`
 	ExpiresAt int64  `json:"expires_at"`
 }
