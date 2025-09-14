5baa94454e817355305dbeed7f81063ef2aaedd8
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-15 00:49:41 +0530

    feat: add DB function for team joins and team_id index

---
+++ database/extras.sql
@@ -0,0 +1,30 @@
+-- ### FUNCTIONS ###
+
+-- function to join a team; enforces team limit
+CREATE OR REPLACE FUNCTION join_team(teamid bigint, user_id bigint, user_limit integer)
+RETURNS void AS $$
+DECLARE
+    user_count integer;
+BEGIN
+    -- Lock the team row to prevent concurrent modifications
+    PERFORM 1 FROM teams WHERE id = teamid FOR UPDATE;
+
+    SELECT INTO user_count COUNT(*)
+    FROM users
+    WHERE team_id = teamid;
+
+    IF user_count >= user_limit THEN
+        RAISE EXCEPTION 'TEAM-04';
+    END IF;
+
+    UPDATE users
+    SET team_id = teamid
+    WHERE id = user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- ### INDEXES ###
+
+-- index for team_id in users table
+CREATE INDEX idx_users_team_id ON users(team_id) WHERE team_id IS NOT NULL;
