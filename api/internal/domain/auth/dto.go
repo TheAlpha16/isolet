b565ac22b66af2487c5280eb6b4e84e813d7f8a5
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-14 12:08:50 +0530

    fix: remove minimum character limits on username

--- api/internal/domain/auth/dto.go
+++ api/internal/domain/auth/dto.go
@@ -16,7 +16,7 @@ func (l *LoginInput) Validate(ctx context.Context) error {
 }
 
 type RegisterInput struct {
-	Username string `json:"username" validate:"required,min=3,max=33"`
+	Username string `json:"username" validate:"required,max=32"`
 	Email    string `json:"email" validate:"required,email,max=320"`
 	Password string `json:"password" validate:"required,min=8,max=32"`
 }
