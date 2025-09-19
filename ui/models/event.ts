39ba3f5bd16ed67885ae9ecaf08456673f43fa41
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-19 23:11:21 +0530

    feat: implement EventInfoUI interface and conversion function from Unix timestamps

---
+++ ui/models/event.ts
@@ -0,0 +1,15 @@
+import { EventInfo } from "@/api/types";
+import { fromUnixSeconds } from "@/utils/helpers";
+
+export interface EventInfoUI extends Omit<EventInfo, "event_start" | "event_end"> {
+  event_start: Date;
+  event_end: Date;
+}
+
+export function toEventInfoUI(eventInfo: EventInfo): EventInfoUI {
+  return {
+    ...eventInfo,
+    event_start: fromUnixSeconds(eventInfo.event_start),
+    event_end: fromUnixSeconds(eventInfo.event_end),
+  };
+}
