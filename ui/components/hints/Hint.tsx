0951c87dc6bdf6f4ed84b103c38d844e8602823c
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-21 02:17:32 +0530

    feat: refactor Hint component to improve type usage and streamline unlock logic

--- ui/components/hints/Hint.tsx
+++ ui/components/hints/Hint.tsx
@@ -1,47 +1,41 @@
-import type { HintType } from "@/utils/types";
-import { Button } from "@/components/ui/button";
-import { Lightbulb, Lock } from "lucide-react";
-import { useChallengeStore } from "@/store/challengeStore";
+import type { Hint } from "@/api";
 import { showHint } from "@/components/hints/HintToastContainer";
-import { useState } from "react";
 import { HintUnlockConfirmation } from "@/components/hints/HintUnlockComponent";
+import { Button } from "@/components/ui/button";
+import { useChallengeStore } from "@/store/challenge";
+import { Lightbulb, Lock } from "lucide-react";
+import { useState } from "react";
 
-function Hint(hint: HintType) {
-	const { unlockHint } = useChallengeStore();
-  	const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+export default function Hint(hint: Hint) {
+  const { unlockHint } = useChallengeStore();
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
 
-	const handleUnlock = () => {
-		setIsConfirmationOpen(true);
-	};
+  const handleUnlock = () => {
+    setIsConfirmationOpen(true);
+  };
 
-	const confirmUnlock = () => {
-		unlockHint(hint.chall_id, hint.hid);
-		setIsConfirmationOpen(false);
-	};
+  const confirmUnlock = () => {
+    unlockHint(hint.challenge_id, hint.id);
+    setIsConfirmationOpen(false);
+  };
 
-	return (
-		<>{
-			hint.unlocked ? (
-			<Button
-				variant={"outline"}
-				size={"sm"}
-				onClick={() => showHint(hint.hint)}
-			><Lightbulb className="text-green-500"/></Button>
-			) : (
-			<Button
-				variant={"secondary"}
-				size={"sm"}
-				onClick={handleUnlock}
-			><Lock className="text-yellow-500"/></Button>
-			)
-		}
-		<HintUnlockConfirmation
-			isOpen={isConfirmationOpen}
-			onClose={() => setIsConfirmationOpen(false)}
-			onConfirm={confirmUnlock}
-			cost={hint.cost}
-		/></>
-	);
-};
-
-export default Hint;
+  return (
+    <>
+      {hint.unlocked ? (
+        <Button variant={"outline"} size={"sm"} onClick={() => showHint(hint.text)}>
+          <Lightbulb className="text-green-500" />
+        </Button>
+      ) : (
+        <Button variant={"secondary"} size={"sm"} onClick={handleUnlock}>
+          <Lock className="text-yellow-500" />
+        </Button>
+      )}
+      <HintUnlockConfirmation
+        isOpen={isConfirmationOpen}
+        onClose={() => setIsConfirmationOpen(false)}
+        onConfirm={confirmUnlock}
+        cost={hint.cost}
+      />
+    </>
+  );
+}
