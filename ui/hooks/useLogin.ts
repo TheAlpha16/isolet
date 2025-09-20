0959f3ca35c579fa53392b834f9d6ad75e2b4ea9
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-20 22:02:20 +0530

    feat: replace old useLogin implementation with a new streamlined version

---
+++ ui/hooks/useLogin.ts
@@ -0,0 +1,30 @@
+import { AuthService } from "@/services";
+import showToast, { ToastStatus } from "@/utils/toastHelper";
+import { useState } from "react";
+
+export default function useLogin() {
+  const [loading, setLoading] = useState(false);
+
+  const login = async (identifier: string, password: string) => {
+    // normalize
+    identifier = identifier.trim();
+    password = password.trim();
+
+    if (!identifier || !password) {
+      showToast(ToastStatus.Failure, "email/username and password are required");
+      return false;
+    }
+
+    setLoading(true);
+    try {
+      await AuthService.login({ identifier, password });
+      return true;
+    } catch (err: any) {
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { login, loading };
+}
