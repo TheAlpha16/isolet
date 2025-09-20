ac59e809749b3e81c998864a02b80f0d53c6eb0c
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-20 21:59:25 +0530

    feat: implement new useForgotPassword, useLogin, and useResetPassword hooks for authentication flow

---
+++ ui/hooks/useForgotPassword.ts
@@ -0,0 +1,29 @@
+import { AuthService } from "@/services";
+import showToast, { ToastStatus } from "@/utils/toastHelper";
+import { useState } from "react";
+
+export default function useForgotPassword() {
+  const [loading, setLoading] = useState(false);
+
+  const forgotPassword = async (email: string) => {
+    // normalize
+    email = email.trim();
+
+    if (!email) {
+      showToast(ToastStatus.Failure, "email is required");
+      return false;
+    }
+
+    setLoading(true);
+    try {
+      await AuthService.forgotPassword({ email });
+      return true;
+    } catch (err: any) {
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { forgotPassword, loading };
+}
---
+++ ui/hooks/useResetPssword.ts
@@ -0,0 +1,40 @@
+import { AuthService } from "@/services";
+import showToast, { ToastStatus } from "@/utils/toastHelper";
+import { useState } from "react";
+
+export default function useResetPassword() {
+  const [loading, setLoading] = useState(false);
+
+  const resetPassword = async (password: string, confirmPassword: string, token: string) => {
+    // normalize
+    password = password.trim();
+    confirmPassword = confirmPassword.trim();
+    token = token.trim();
+
+    if (!token) {
+      showToast(ToastStatus.Failure, "invalid token");
+    }
+
+    if (!password || !confirmPassword) {
+      showToast(ToastStatus.Failure, "all fields are required");
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      showToast(ToastStatus.Failure, "passwords do not match");
+      return false;
+    }
+
+    setLoading(true);
+    try {
+      await AuthService.resetPassword({ password, token });
+      return true;
+    } catch (err: any) {
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { resetPassword, loading };
+}
