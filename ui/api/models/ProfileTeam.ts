74a9233531cbbc9f4b69b9bde465fd3465d22541
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-21 14:32:38 +0530

    feat: add members field to ProfileTeam type and update OpenAPI schema

--- ui/api/models/ProfileTeam.ts
+++ ui/api/models/ProfileTeam.ts
@@ -4,9 +4,11 @@
 /* eslint-disable */
 import type { Submission } from './Submission';
 import type { Team } from './Team';
+import type { User } from './User';
 export type ProfileTeam = (Team & {
     score: number;
     rank?: number | null;
+    members: Array<User>;
     submissions: Array<Submission>;
 });
 
