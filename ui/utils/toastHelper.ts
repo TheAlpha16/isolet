cfb395cd38f105eff081eb3d2b842fba5aadb3ed
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-21 17:37:56 +0530

    ref: standardize formatting in toastHelper utility

--- ui/utils/toastHelper.ts
+++ ui/utils/toastHelper.ts
@@ -1,27 +1,27 @@
-'use client'
+"use client";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 export enum ToastStatus {
-    Success,
-    Warning,
-    Failure
+  Success,
+  Warning,
+  Failure,
 }
 
 function showToast(status: ToastStatus, message: string) {
-    switch (status) {
-        case ToastStatus.Success:
-            toast.success(message, { containerId: 'notification-toast' })
-            break;
-        case ToastStatus.Failure:
-            toast.error(message, { containerId: 'notification-toast' })
-            break;
-        case ToastStatus.Warning:
-            toast.warn(message, { containerId: 'notification-toast' })
-            break;
-        default:
-            toast.warn(message, { containerId: 'notification-toast' })
-    }
+  switch (status) {
+    case ToastStatus.Success:
+      toast.success(message, { containerId: "notification-toast" });
+      break;
+    case ToastStatus.Failure:
+      toast.error(message, { containerId: "notification-toast" });
+      break;
+    case ToastStatus.Warning:
+      toast.warn(message, { containerId: "notification-toast" });
+      break;
+    default:
+      toast.warn(message, { containerId: "notification-toast" });
+  }
 }
 
 export default showToast;
