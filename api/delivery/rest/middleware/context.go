7b42285b82c20c53c4cd1868c6e42d37602ff8c7
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-28 01:08:12 +0530

    feat: add ContextDeadlineMiddleware for request timeout management

--- api/delivery/rest/middleware/context.go
+++ api/delivery/rest/middleware/context.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"context"
+	"time"
+
 	"github.com/TheAlpha16/isolet/api/internal/domain/common"
 	errorDom "github.com/TheAlpha16/isolet/api/internal/domain/errors"
 
@@ -13,6 +16,16 @@ func ContextMiddleware() fiber.Handler {
 		ctx := c.UserContext()
 		ctx = common.SetExtraDataInCtx(ctx, make(common.ExtraData))
 		ctx = errorDom.SetSrcInCtx(ctx, c.Path()) // Set source to the path
+		c.SetUserContext(ctx)
+
+		return c.Next()
+	}
+}
+
+func ContextDeadlineMiddleware(duration time.Duration) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ctx, cancel := context.WithTimeout(c.UserContext(), duration)
+		defer cancel()
 		c.SetUserContext(ctx)
 
 		return c.Next()
