c2d4c417b0d1b39f9c53b2c7d0997bb91fb265b9
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-09-29 01:04:24 +0530

    feat: add Instance CRD, update role permissions, and implement deep copy methods for instance types

--- tide/api/v1/instance_types.go
+++ tide/api/v1/instance_types.go
@@ -17,11 +17,159 @@ limitations under the License.
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// Challenge type
+// +kubebuilder:validation:Enum=dynamic;on-demand
+type ChallengeType string
+
+const (
+	// ChallengeTypeDynamic indicates a challenge that is always running until manually terminated (common instance for all teams)
+	ChallengeTypeDynamic ChallengeType = "dynamic"
+
+	// ChallengeTypeOnDemand indicates a challenge that is provisioned on-demand and terminated after use (isolated instance for a team)
+	ChallengeTypeOnDemand ChallengeType = "on-demand"
+)
+
+// Protocol indicates the protocol or service type of an endpoint
+// +kubebuilder:validation:Enum=http;https;nc;ssh
+type Protocol string
+
+const (
+	// ProtocolHTTP indicates HTTP protocol
+	ProtocolHTTP Protocol = "http"
+
+	// ProtocolHTTPS indicates HTTPS protocol
+	ProtocolHTTPS Protocol = "https"
+
+	// ProtocolNC indicates Netcat protocol
+	ProtocolNC Protocol = "nc"
+
+	// ProtocolSSH indicates SSH protocol
+	ProtocolSSH Protocol = "ssh"
+)
+
+// Phase indicates the current phase of the instance
+// Possible values:
+// - Pending: Instance is being created
+// - Staged: Instance is running but not accepting traffic yet (can be configured via lifecycle.availableAt)
+// - Running: Instance is running and accepting traffic
+// - Failed: Instance creation or startup failed
+//
+// +kubebuilder:validation:Enum=Pending;Staged;Running;Failed
+type Phase string
+
+const (
+	// PhasePending indicates the instance is being created
+	PhasePending Phase = "Pending"
+
+	// PhaseStaged indicates the instance is running but not accepting traffic yet
+	PhaseStaged Phase = "Staged"
+
+	// PhaseRunning indicates the instance is running and accepting traffic
+	PhaseRunning Phase = "Running"
+
+	// PhaseFailed indicates the instance creation or startup failed
+	PhaseFailed Phase = "Failed"
+)
+
+// Challenge object defines configuration for the challenge for which the instance is created
+type Challenge struct {
+	// ID of the challenge
+	// +required
+	// +kubebuilder:validation:Minimum=1
+	ID int64 `json:"id"`
+
+	// DNS-safe slug derived from challenge name
+	// ex: Alice's Adventure -> alices-adventure
+	// +required
+	Name string `json:"name"`
+
+	// A custom and unique flag can be passed per instance
+	// If set, it will be injected as an environment variable into the container
+	// +optional
+	Flag *string `json:"flag,omitempty"`
+
+	// type indicates the provisioning type of the challenge
+	// +required
+	Type ChallengeType `json:"type"`
+
+	// Docker image to use for the challenge instance
+	// +required
+	Image string `json:"image"`
+}
+
+// Team refers to the team for which the instance is created
+type Team struct {
+	// ID of the team
+	// +required
+	// +kubebuilder:validation:Minimum=1
+	ID int64 `json:"id"`
+}
+
+// Endpoint represents a network endpoint for an instance
+type Endpoint struct {
+	// Name of the endpoint (e.g., "app", "metrics")
+	// +optional
+	Name *string `json:"name,omitempty"`
+
+	// Protocol of the endpoint (e.g., "http", "ssh")
+	// +required
+	Protocol Protocol `json:"protocol"`
+
+	// Hostname of the endpoint
+	// Controller genarates a DNS-safe hostname if not provided
+	// e.g., <instance-uuid>.<challenge-name>.<tide-domain>
+	//
+	// Example: "a1b2c3d4-e5f6-7890-abcd-ef1234567890.alices-adventure.isolet.dev"
+	// +optional
+	Hostname *string `json:"hostname,omitempty"`
+
+	// Port of the endpoint
+	// This is resolved by the controller based on the service configuration
+	// +optional
+	Port *int32 `json:"port"`
+
+	// TargetPort of the endpoint
+	// Port on which the application inside the container is listening
+	// +required
+	TargetPort int32 `json:"targetPort"`
+
+	// Ready indicates whether the endpoint is ready to accept traffic
+	// +optional
+	Ready *bool `json:"ready"`
+}
+
+// Lifecycle defines the lifecycle configuration for an instance
+type Lifecycle struct {
+	// timestamp of the time when the instance should be available
+	// If not set, the instance will be available immediately after provisioning
+	// +optional
+	AvailableAt *metav1.Time `json:"availableAt,omitempty"`
+
+	// timestamp of the time when the instance should be terminated
+	// If not set, the instance will run indefinitely until manually terminated
+	// +optional
+	ExpiresAt *metav1.Time `json:"expiresAt,omitempty"`
+
+	// allowExtension indicates whether the instance's expiry time can be extended by the user
+	// Default is true
+	// +optional
+	// +kubebuilder:default=true
+	AllowExtension bool `json:"allowExtension"`
+
+	// restartPolicy defines the restart policy for all containers within the instance
+	// One of Always, OnFailure, Never
+	// Default is Never
+	// +optional
+	// +kubebuilder:default="Never"
+	RestartPolicy corev1.RestartPolicy `json:"restartPolicy,omitempty"`
+}
 
 // InstanceSpec defines the desired state of Instance
 type InstanceSpec struct {
@@ -30,15 +178,44 @@ type InstanceSpec struct {
 	// The following markers will use OpenAPI v3 schema to validate the value
 	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 
-	// foo is an example field of Instance. Edit instance_types.go to remove/update
+	// challenge is the challenge for which this instance is created
+	// +required
+	Challenge Challenge `json:"challenge"`
+
+	// team is the team for which this instance is created
+	// dynamic challenges may not have a team associated
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Team *Team `json:"team,omitempty"`
+
+	// requests is the resource requests for the instance
+	// +optional
+	Requests corev1.ResourceList `json:"requests,omitempty"`
+
+	// limits is the resource limits for the instance
+	// +optional
+	Limits corev1.ResourceList `json:"limits,omitempty"`
+
+	// Endpoints represents the network endpoints for the instance
+	// +optional
+	Endpoints []Endpoint `json:"endpoints,omitempty"`
+
+	// lifecycle defines the lifecycle configuration for the instance
+	// +optional
+	Lifecycle *Lifecycle `json:"lifecycle,omitempty"`
 }
 
 // InstanceStatus defines the observed state of Instance.
 type InstanceStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// phase indicates the current phase of the instance
+	// +optional
+	Phase Phase `json:"phase,omitempty"` // Pending | Staged | Running | Failed
+
+	// endpoints are the resolved network endpoints for the instance
+	// +optional
+	Endpoints []Endpoint `json:"endpoints,omitempty"`
 
 	// For Kubernetes API conventions, see:
 	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
