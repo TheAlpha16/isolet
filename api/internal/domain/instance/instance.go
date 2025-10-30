89d31fefebd4b921f4a2d1baf3d97c73ed135700
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-10-30 23:50:26 +0530

    fix: cleam up redundant models

--- api/internal/domain/instance/instance.go
+++ api/internal/domain/instance/instance.go
@@ -6,50 +6,16 @@ import (
 	"github.com/TheAlpha16/isolet/api/internal/domain"
 )
 
-type Protocol string
-
 const (
 	InstanceCachePrefix = "instance"
 )
 
-const (
-	ProtocolHTTP  Protocol = "http"
-	ProtocolHTTPS Protocol = "https"
-	ProtocolNC    Protocol = "nc"
-	ProtocolSSH   Protocol = "ssh"
-)
-
 type Instance struct {
-	ID          int64
-	ChallengeID int64
-	TeamID      int64
-	ExpiresAt   int64
-	Info        *DeploymentInfo
+	ID        int64
+	TeamID    int64
+	Manifest  *Manifest
+	Lifecycle *Lifecycle
 	domain.BaseEntity
-}
-
-type DeploymentInfo struct {
-	ID         int64
-	Slug       string
-	Image      string
-	TargetPort int32
-	Requests   []*Resource
-	Limits     []*Resource
-	Endpoints  []*Endpoint
-}
-
-type Resource struct {
-	Name  string
-	Value string
-}
-
-type Endpoint struct {
-	ID         int64
-	Name       string
-	Protocol   Protocol
-	TargetPort int32
-	Hostname   *string
-	Port       *int32
 }
 
 func InstanceCacheKey(teamID, challengeID int64) string {
