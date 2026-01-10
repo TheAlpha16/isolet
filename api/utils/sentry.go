98e36c752d3ec85d9597fcd664c81f5dfbc71b36
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-10 12:52:34 +0530

    feat: bump up semconv package to v1.37.0

--- api/utils/sentry.go
+++ api/utils/sentry.go
@@ -6,7 +6,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
 
 func InitSentry(config *Config) {
