3e47367ca263aec3ddf6000fb874fb5c6a7d1a06
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-17 18:56:58 +0530

    feat[herald]: new fact errors

--- herald/utils/errors/constants.go
+++ herald/utils/errors/constants.go
@@ -10,7 +10,9 @@ const (
 	ErrInternalError ErrorCode = "SYS-00"
 
 	// Fact errors
-	ErrFactInvalidKey ErrorCode = "FACT-01"
+	ErrFactInvalidKey       ErrorCode = "FACT-01"
+	ErrFactInvalidType      ErrorCode = "FACT-02"
+	ErrFactInvalidOccuredAt ErrorCode = "FACT-03"
 )
 
 // Map of error codes to user-facing messages
@@ -19,5 +21,7 @@ var msgMap = map[ErrorCode]string{
 	ErrInternalError: "internal server error",
 
 	// Fact errors
-	ErrFactInvalidKey: "fact has invalid key",
+	ErrFactInvalidKey:       "fact has invalid key",
+	ErrFactInvalidType:      "fact has invalid type",
+	ErrFactInvalidOccuredAt: "fact has invalid occured at timestamp",
 }
