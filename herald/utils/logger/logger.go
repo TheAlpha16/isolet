41d0589c303c1e0764ed722198bff4857a923cfd
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-17 15:22:44 +0530

    feat[herald]: implement logger utility with zap for structured logging

---
+++ herald/utils/logger/logger.go
@@ -0,0 +1,77 @@
+package logger
+
+import (
+	"context"
+	"sync"
+
+	"github.com/TheAlpha16/isolet/herald/utils"
+	"github.com/TheAlpha16/isolet/herald/utils/errors"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	logger *StandardLogger
+	once   sync.Once
+)
+
+type StandardLogger struct {
+	*zap.Logger
+}
+
+func Init() {
+	once.Do(func() {
+		logger = New()
+	})
+}
+
+func New() *StandardLogger {
+	var cfg zap.Config
+	config := utils.GetConfig()
+	outputLevel, err := zapcore.ParseLevel(config.LogLevel)
+	if err != nil {
+		outputLevel = zapcore.InfoLevel
+	}
+
+	if config.Environment != utils.LOCAL {
+		cfg = zap.NewProductionConfig()
+		cfg.Level = zap.NewAtomicLevelAt(outputLevel)
+		cfg.Encoding = "json"
+	} else {
+		cfg = zap.NewDevelopmentConfig()
+	}
+
+	cfg.OutputPaths = []string{"stdout"}
+	cfg.ErrorOutputPaths = []string{"stdout"}
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	cfg.EncoderConfig.TimeKey = "time"
+	cfg.DisableStacktrace = true
+
+	zapLogger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	newLogger := &StandardLogger{zapLogger}
+	newLogger.Info("Logger initialized", zap.String("level", outputLevel.String()))
+
+	return newLogger
+}
+
+func (l *StandardLogger) WithFields(fields ...zapcore.Field) *StandardLogger {
+	return &StandardLogger{l.With(fields...)}
+}
+
+func (l *StandardLogger) Sync() {
+	if err := l.Logger.Sync(); err != nil {
+		errors.RaiseToSentry(context.Background(), err)
+	}
+}
+
+func GetAppLogger() *StandardLogger {
+	if logger == nil {
+		Init()
+	}
+	return logger
+}
