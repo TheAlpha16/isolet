4a1daa1d770c3031e1e1b808fad212a3c1b992b8
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-17 15:00:54 +0530

    feat: integrate Sentry for error tracking and monitoring

--- herald/utils/config.go
+++ herald/utils/config.go
@@ -24,6 +24,12 @@ type Config struct {
 	Environment Environment `env:"ENVIRONMENT" envDefault:"local"`
 	Version     string      `env:"VERSION" envDefault:"0.1.0"`
 	LogLevel    string      `env:"LOG_LEVEL" envDefault:"DEBUG"`
+
+	Sentry struct {
+		DSN             string  `env:"SENTRY_DSN" envDefault:""`
+		SampleRate      float64 `env:"SENTRY_SAMPLE_RATE" envDefault:"1.0"`
+		TraceSampleRate float64 `env:"SENTRY_TRACE_SAMPLE_RATE" envDefault:"0.05"`
+	}
 }
 
 func GetConfig() *Config {
---
+++ herald/utils/sentry.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"github.com/getsentry/sentry-go"
+	sentryotel "github.com/getsentry/sentry-go/otel"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+)
+
+func InitSentry(config *Config) {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              config.Sentry.DSN,
+		AttachStacktrace: true,
+		SampleRate:       config.Sentry.SampleRate,
+		EnableTracing:    true,
+		TracesSampleRate: config.Sentry.TraceSampleRate,
+		ServerName:       config.Name,
+		Release:          config.Version,
+		Environment:      string(config.Environment),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceName(config.Name)),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSpanProcessor(sentryotel.NewSentrySpanProcessor()),
+		sdktrace.WithResource(r),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(sentryotel.NewSentryPropagator())
+}
