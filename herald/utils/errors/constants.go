4d81e6e07ce6991aa185d2beea0aaafa2e5568cd
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-17 18:31:10 +0530

    fix[herald]: add error code for invalid fact key

--- herald/utils/errors/constants.go
+++ herald/utils/errors/constants.go
@@ -7,11 +7,17 @@ const (
 // Error codes
 const (
 	// System errors
-	ErrInternalError  ErrorCode = "SYS-00"
+	ErrInternalError ErrorCode = "SYS-00"
+
+	// Fact errors
+	ErrFactInvalidKey ErrorCode = "FACT-01"
 )
 
 // Map of error codes to user-facing messages
 var msgMap = map[ErrorCode]string{
 	// System errors
 	ErrInternalError: "internal server error",
+
+	// Fact errors
+	ErrFactInvalidKey: "fact has invalid key",
 }
