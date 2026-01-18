2169d4868121a6fba0f9ac714609f9683c3a001f
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-18 23:05:01 +0530

    ref[api]: simplify fact types by using definitions from the herald package

--- api/internal/domain/fact/fact.go
+++ api/internal/domain/fact/fact.go
@@ -1,63 +1,15 @@
 package fact
 
 import (
-	"fmt"
-	"time"
+	"github.com/TheAlpha16/isolet/herald/pkg/facts"
 )
+
+type FactType = facts.FactType
 
 const (
-	InstanceExpiredFactType FactType = "InstanceExpired"
+	InstanceExpiredFactType FactType = facts.InstanceExpiredFactType
 )
 
-type FactType string
-
-type Fact interface {
-	FactType() FactType
-	Key() []byte
-	OccuredAt() time.Time
-	Validate() error
-}
-
-type BaseFact struct {
-	Type FactType  `json:"type"`
-	At   time.Time `json:"at"`
-}
-
-func (f *BaseFact) FactType() FactType {
-	return f.Type
-}
-
-func (f *BaseFact) OccuredAt() time.Time {
-	return f.At
-}
-
-func (f *BaseFact) Validate() error {
-	if f.Type == "" {
-		return fmt.Errorf("fact type is required")
-	}
-	if f.At.IsZero() {
-		return fmt.Errorf("fact occured at is required")
-	}
-	return nil
-}
-
-type InstanceFact struct {
-	BaseFact
-	ID          string `json:"id"`
-	ChallengeID int64  `json:"challenge_id"`
-	TeamID      *int64 `json:"team_id"`
-}
-
-func (f *InstanceFact) Key() []byte {
-	return []byte(f.ID)
-}
-
-func (f *InstanceFact) Validate() error {
-	if err := f.BaseFact.Validate(); err != nil {
-		return err
-	}
-	if f.ID == "" {
-		return fmt.Errorf("instance fact ID is required")
-	}
-	return nil
-}
+type Fact = facts.Fact
+type BaseFact = facts.BaseFact
+type InstanceFact = facts.InstanceFact
