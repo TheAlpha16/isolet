78def6ac1b660509c768e3f630b2f2ab6dc1f1f8
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-18 22:53:46 +0530

    feat[api]: implement Fact and BaseFact types with validation methods

--- api/internal/domain/fact/interface.go
+++ api/internal/domain/fact/interface.go
@@ -2,17 +2,7 @@ package fact
 
 import (
 	"context"
-	"time"
 )
-
-type FactType string
-
-type Fact interface {
-	FactType() FactType
-	Key() []byte
-	OccuredAt() time.Time
-	Validate() error
-}
 
 type Usecase interface {
 	HandleEvent(ctx context.Context, fact Fact) error
