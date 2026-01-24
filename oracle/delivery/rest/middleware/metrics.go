981ab4098dd1daa725a8a1715b1ea389e84c9ec7
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-24 19:30:12 +0530

    ref[oracle]: enhance MetricsMiddleware for improved readability and consistency

--- oracle/delivery/rest/middleware/metrics.go
+++ oracle/delivery/rest/middleware/metrics.go
@@ -2,29 +2,38 @@ package middleware
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/TheAlpha16/isolet/oracle/utils/tracer"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
 )
 
 func MetricsMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		start := time.Now()
-		method := c.Method()
-		path := c.Route().Path
+		method := utils.CopyString(c.Method())
+		path := utils.CopyString(c.Path())
+
+		// DEBUG
+		fmt.Println("[MetricsMiddleware] method:", method, "path:", path)
 
 		tracer.HttpRequestsInFlight.WithLabelValues(path).Inc()
 		defer tracer.HttpRequestsInFlight.WithLabelValues(path).Dec()
 
 		err := c.Next()
 
-		status := c.Response().StatusCode()
-		duration := time.Since(start).Seconds()
-		statusStr := fmt.Sprintf("%d", status)
-
-		tracer.HttpRequestsTotal.WithLabelValues(method, path, statusStr).Inc()
-		tracer.HttpRequestDurationSeconds.WithLabelValues(method, path).Observe(duration)
+		tracer.HttpRequestsTotal.WithLabelValues(
+			method,
+			path,
+			strconv.Itoa(c.Response().StatusCode()),
+		).Inc()
+		tracer.HttpRequestDurationSeconds.WithLabelValues(
+			method,
+			path,
+		).Observe(time.Since(start).Seconds())
 
 		return err
 	}
