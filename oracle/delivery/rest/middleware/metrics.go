e5e4a6a47924eb50c3aeebf859abde5f5796fc9d
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-24 20:15:56 +0530

    ref[oracle]: skip health and metrics route from recording metrics

--- oracle/delivery/rest/middleware/metrics.go
+++ oracle/delivery/rest/middleware/metrics.go
@@ -4,17 +4,27 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/TheAlpha16/isolet/oracle/utils"
 	"github.com/TheAlpha16/isolet/oracle/utils/tracer"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/utils"
+	fiberutils "github.com/gofiber/fiber/v2/utils"
 )
+
+var skipMetricRoutes = map[string]struct{}{
+	utils.RouteMetrics: {},
+	utils.RoutePing:    {},
+}
 
 func MetricsMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		start := time.Now()
-		method := utils.CopyString(c.Method())
-		path := utils.CopyString(c.Path())
+		method := fiberutils.CopyString(c.Method())
+		path := fiberutils.CopyString(c.Path())
+
+		if _, skip := skipMetricRoutes[path]; skip {
+			return c.Next()
+		}
 
 		tracer.HttpRequestsInFlight.WithLabelValues(path).Inc()
 		defer tracer.HttpRequestsInFlight.WithLabelValues(path).Dec()
