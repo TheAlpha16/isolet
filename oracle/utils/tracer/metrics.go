7c10e072a45eab833b60ed27e7d4912ad4d3d636
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-24 18:05:15 +0530

    feat[oracle]: add HTTP metrics for request tracking and performance

--- oracle/utils/tracer/metrics.go
+++ oracle/utils/tracer/metrics.go
@@ -57,6 +57,32 @@ var (
 		},
 		[]string{"status"},
 	)
+
+	// HTTP
+	HttpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	HttpRequestDurationSeconds = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests",
+			Buckets: []float64{0.1, 0.5, 1, 2, 5},
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	HttpRequestsInFlight = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "http_requests_in_flight",
+			Help: "Current number of in-flight HTTP requests",
+		},
+		[]string{"method", "path"},
+	)
 )
 
 func InitMetrics() error {
