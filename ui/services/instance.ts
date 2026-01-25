923302880702b5ee125b735e1ee51c01e87cfd0a
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-01-25 21:04:28 +0530

    feat[ui]: add methods for starting, stopping, and extending instances in InstanceService

--- ui/services/instance.ts
+++ ui/services/instance.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Instance } from "@/api";
+import type { Instance, InstanceStartInput, InstanceStopInput, InstanceExtendInput } from "@/api";
 import { InstanceService as ApiInstanceService } from "../api/services/InstanceService";
 import { BaseService } from "./base";
 
@@ -11,5 +11,32 @@ export class InstanceService extends BaseService {
    */
   public static async getInstances(): Promise<Instance[] | undefined> {
     return this.handleResponse<Instance[]>(ApiInstanceService.getInstance());
+  }
+
+  /**
+   * Start instance
+   * Starts a new instance for a challenge
+   */
+  public static async startInstance(challenge_id: number): Promise<Instance | undefined> {
+    const requestBody: InstanceStartInput = { challenge_id };
+    return this.handleResponse<Instance>(ApiInstanceService.postInstanceStart(requestBody));
+  }
+
+  /**
+   * Stop instance
+   * Stops a running instance
+   */
+  public static async stopInstance(instance_id: number): Promise<void> {
+    const requestBody: InstanceStopInput = { instance_id };
+    await this.handleResponse(ApiInstanceService.postInstanceStop(requestBody));
+  }
+
+  /**
+   * Extend instance
+   * Extends the lifetime of a running instance
+   */
+  public static async extendInstance(instance_id: number): Promise<Instance | undefined> {
+    const requestBody: InstanceExtendInput = { instance_id };
+    return this.handleResponse<Instance>(ApiInstanceService.postInstanceExtend(requestBody));
   }
 }
