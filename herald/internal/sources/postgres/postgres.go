ae2a1cbf95fdb95743b47277c66b59e6ad8d687e
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-02-07 15:32:30 +0530

    feat[herald]: enhance pgSource structure with additional fields

--- herald/internal/sources/postgres/postgres.go
+++ herald/internal/sources/postgres/postgres.go
@@ -7,9 +7,23 @@ import (
 	"github.com/TheAlpha16/isolet/herald/internal/sources"
 	"github.com/TheAlpha16/isolet/herald/pkg/facts"
 	"github.com/TheAlpha16/isolet/herald/utils"
+
+	"github.com/jackc/pglogrepl"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	slotName        = "herald_slot"
+	publicationName = "herald_pub"
 )
 
 type pgSource struct {
+	sqlPool *pgxpool.Pool
+	repl    *pgconn.PgConn
+	lastLSN pglogrepl.LSN
+
+	wg *sync.WaitGroup
 }
 
 func (s *pgSource) Name() string {
@@ -21,5 +35,7 @@ func (s *pgSource) Run(ctx context.Context, out chan<- facts.Fact) error {
 }
 
 func NewSource(ctx context.Context, wg *sync.WaitGroup) sources.Source {
-	return &pgSource{}
+	return &pgSource{
+		wg: wg,
+	}
 }
