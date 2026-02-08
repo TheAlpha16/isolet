89fd6407dce7f4716cef085f8f5c0ded9c1b2874
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-02-08 17:45:38 +0530

    ref[herald]: move handleReplication function to handlers.go for better organization

--- herald/internal/sources/postgres/postgres.go
+++ herald/internal/sources/postgres/postgres.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/TheAlpha16/isolet/herald/internal/sources"
 	"github.com/TheAlpha16/isolet/herald/pkg/facts"
@@ -15,7 +14,6 @@ import (
 	"github.com/jackc/pglogrepl"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/jackc/pgx/v5/pgproto3"
 	"go.opentelemetry.io/otel"
 	"go.uber.org/zap"
 )
@@ -98,63 +96,6 @@ func (s *pgSource) Stop(ctx context.Context) error {
 	}
 
 	return nil
-}
-
-func (s *pgSource) handleReplication(ctx context.Context, out chan<- facts.Fact) error {
-	ctx, span, log := tracer.StartSpan(ctx, pgTracer, "herald.sources.postgres.handleReplication")
-	defer span.End()
-
-	standbyTimeout := time.NewTicker(utils.GetConfig().Postgres.StandbyTimeout)
-	defer standbyTimeout.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-
-		case <-standbyTimeout.C:
-			// periodic standby update (heartbeat)
-			if s.lastLSN > 0 {
-				s.sendStandby(ctx, span, log)
-			}
-
-		default:
-			msg, err := s.repl.ReceiveMessage(ctx)
-			if err != nil {
-				return err
-			}
-
-			switch m := msg.(type) {
-			case *pgproto3.CopyData:
-				// handle replication messages in the CopyData response
-
-				switch m.Data[0] {
-				case pglogrepl.XLogDataByteID:
-					logData, err := pglogrepl.ParseXLogData(m.Data[1:])
-					if err != nil {
-						continue
-					}
-					s.lastLSN = logData.WALStart + pglogrepl.LSN(len(logData.WALData))
-
-					fmt.Printf("Received WAL data at LSN %s\n", logData.WALStart)
-					fmt.Printf("Raw: %v\n", logData.WALData)
-
-					s.sendStandby(ctx, span, log)
-
-				case pglogrepl.PrimaryKeepaliveMessageByteID:
-					keepAliveMsg, err := pglogrepl.ParsePrimaryKeepaliveMessage(m.Data[1:])
-					if err != nil {
-						continue
-					}
-					if keepAliveMsg.ReplyRequested {
-						s.sendStandby(ctx, span, log)
-						standbyTimeout.Reset(utils.GetConfig().Postgres.StandbyTimeout)
-					}
-				}
-			default:
-			}
-		}
-	}
 }
 
 func NewSource(ctx context.Context, wg *sync.WaitGroup) sources.Source {
