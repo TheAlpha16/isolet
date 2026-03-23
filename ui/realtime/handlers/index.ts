2c254a3b681b560fcd2e074d3196b2f771c4a71e
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-03-24 00:40:33 +0530

    feat[ui]: include endpoint handlers in the real-time updates handler

--- ui/realtime/handlers/index.ts
+++ ui/realtime/handlers/index.ts
@@ -1,7 +1,9 @@
 import { instanceHandlers } from "@/realtime/handlers/instance";
+import { endpointHandlers } from "@/realtime/handlers/endpoint";
 import { notificationHandlers } from "@/realtime/handlers/notification";
 
 export const handlers: Record<string, (payload: any) => void> = {
   ...instanceHandlers,
+  ...endpointHandlers,
   ...notificationHandlers,
 };
