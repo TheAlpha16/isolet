1c55a32db33b36016e7de22729025da92a570c86
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-03-23 22:55:12 +0530

    feat[ui]: add token parameter to socket connection using getCookie

--- ui/realtime/socket.ts
+++ ui/realtime/socket.ts
@@ -1,3 +1,6 @@
 import { Socket } from "phoenix";
+import { getCookie } from "@/utils/helpers";
 
-export const socket = new Socket("/socket", {});
+export const socket = new Socket("/socket", {
+  params: { token: getCookie("realtime_token") },
+});
--- ui/utils/helpers.ts
+++ ui/utils/helpers.ts
@@ -1,3 +1,17 @@
 export function fromUnixSeconds(sec: number): Date {
   return new Date(sec * 1000);
 }
+export function getCookie(name: string): string | undefined {
+  if (typeof document === "undefined") return undefined;
+
+  const cookies = document.cookie.split("; ");
+
+  for (const cookie of cookies) {
+    const [key, ...rest] = cookie.split("=");
+    if (key === name) {
+      return decodeURIComponent(rest.join("="));
+    }
+  }
+
+  return undefined;
+}
