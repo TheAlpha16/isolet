fbe19f7689dbcf8dfebaa3bff82ab93a2c91fb0e
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-03-24 21:31:13 +0530

    feat[oracle]: enhance CheckTimingsMiddleware to validate event timing conditions

--- oracle/delivery/rest/middleware/timings.go
+++ oracle/delivery/rest/middleware/timings.go
@@ -1,13 +1,35 @@
 package middleware
 
 import (
+	"time"
+
 	cvDom "github.com/TheAlpha16/isolet/oracle/internal/domain/configvars"
+	errorDom "github.com/TheAlpha16/isolet/oracle/internal/domain/errors"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func CheckTimingsMiddleware(cvUc cvDom.Usecase) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		ctx := c.UserContext()
+		currentTime := int(time.Now().Unix())
+
+		eventStart := cvUc.GetInt(ctx, cvDom.EventStart)
+		eventEnd := cvUc.GetInt(ctx, cvDom.EventEnd)
+		postEvent := cvUc.GetBool(ctx, cvDom.EventPostMode)
+
+		if postEvent {
+			return c.Next()
+		}
+
+		if currentTime < eventStart {
+			return errorDom.Raise(ctx, errorDom.ErrEventNotStarted, "", nil, nil)
+		}
+
+		if currentTime > eventEnd {
+			return errorDom.Raise(ctx, errorDom.ErrEventEnded, "", nil, nil)
+		}
+
 		return c.Next()
 	}
 }
