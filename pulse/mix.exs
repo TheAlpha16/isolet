d312a3b8bbf099e294f87c1fc3707d6e4884cebe
TheAlpha16 <uddagirijithendranadh@gmail.com>
2026-04-17 22:38:35 +0530

    fix[pulse]: use hexpm/elixir:1.19.5 + ELIXIR_ERL_OPTIONS=+Bi to fix OTP 28 Docker build stdin issue

--- pulse/mix.exs
+++ pulse/mix.exs
@@ -43,8 +43,7 @@ defmodule Pulse.MixProject do
       {:joken, "~> 2.6"},
       {:jose, "~> 1.11"},
       {:redix, "~> 1.5"},
-      {:phoenix_pubsub_redis, "~> 3.0"},
-      {:nimble_options, "~> 1.0.0", override: true}
+      {:phoenix_pubsub_redis, "~> 3.0"}
     ]
   end
 
